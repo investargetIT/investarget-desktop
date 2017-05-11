e61cf759fc0fdb6a2cbc327f4e01d331e71d818b
Peter Xu <xzm920@163.com>
2017-05-11 13:58:25 +0800

    添加路由跳转动画

--- src/router.js
+++ src/router.js
@@ -12,18 +12,44 @@ import RecommendFriends from "./routes/RecommendFriends.js";
 
 import RecommendProjects from "./routes/RecommendProjects.js";
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+function Container(props) {
+  const isPathAllowed = /\/(recommend_friends|recommend_projects)/
+
+  return (
+    <ReactCSSTransitionGroup
+        transitionName="transitionWrapper"
+        component="div"
+        className="transitionWrapper"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        <div key={props.location.pathname}
+              className={ isPathAllowed.test(props.location.pathname) ? '' : 'disabled' }
+              style={{position:"absolute", width: "100%"}}>
+            {
+                props.children
+            }
+        </div>
+    </ReactCSSTransitionGroup>
+  )
+}
+
+
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/app" component={IndexPage} />
-      <Route path="/app/products" component={Products} />
-      <Route path="/app/users" component={Users} />
-      <Route path="/app/investor/list" component={InvestorList} />
-      <Route path="/recommend_friends" component={RecommendFriends} />
-      <Route path="/recommend_projects" component={RecommendProjects} />
+      <div component={Container}>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/app" component={IndexPage} />
+        <Route path="/app/products" component={Products} />
+        <Route path="/app/users" component={Users} />
+        <Route path="/app/investor/list" component={InvestorList} />
+        <Route path="/recommend_friends" component={RecommendFriends} />
+        <Route path="/recommend_projects" component={RecommendProjects} />
+      </div>
     </Router>
   );
 }
