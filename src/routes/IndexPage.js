4dafba5892882a12103bf8212d0ddfb7df01b64b
Oliver <olimovier@gmail.com>
2017-06-20 19:22:36 +0800

    Import Recharts to render charts

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,21 +2,55 @@ import React from 'react'
 import { connect } from 'dva'
 import MainLayout from '../components/MainLayout'
 import { Card, Col } from 'antd'
+import { BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+
+const data = [
+  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+]
+
+const barData = [
+  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+]
 
 function IndexPage({ location }) {
   return (
     <MainLayout location={location} style={{}}>
       <Col span={16}>
-        <Card title="Card title 1" bordered={false} style={{ margin: '10px 0 0 10px' }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Catdd dsfdff</p>
+        <Card title="SimpleLineChart" bordered={false} style={{ margin: '10px 0 0 10px' }}>
+          <LineChart style={{ margin: '0 auto' }} width={600} height={300} data={data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="name"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip/>
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
         </Card>
-        <Card title="Card title 2" bordered={false} style={{ margin: '10px 0 0 10px' }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
+        <Card title="SimpleBarChart" bordered={false} style={{ margin: '10px 0 0 10px' }}>
+          <BarChart style={{ margin: '0 auto' }} width={600} height={300} data={barData}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="name"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip/>
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
         </Card>
         <Card title="Card title 3" bordered={false} style={{ margin: '10px 0 0 10px' }}>
           <p>Card content</p>
