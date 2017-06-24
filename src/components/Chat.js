e15a7b66116d18df50979ec7dd1535914e3ba413
Oliver <olimovier@gmail.com>
2017-06-24 14:23:47 +0800

    Basically complete the chat ui

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,110 @@
 import React from 'react'
 
-const topBarHeight = 38
+const mySpeechBubbleColor = 'rgb(162, 229, 99)'
+//const mySpeechBubbleColor = 'lightBlue'
+
+
+function SpeechOfMy(props) {
+  return (
+    <div style={{ overflow: 'auto' }}>
+    <div style={{ float: 'right' }}><img style={{ width: 30, height: 30, borderRadius: 2 }} src="/images/default-avatar.png" /></div>
+    <div className="my-talk-bubble" style={{ float: 'right', maxWidth: '75%', marginRight: 8, position: 'relative', border: `1px solid ${mySpeechBubbleColor}`, borderRadius: 4 }}>
+      <div style={{ padding: '6px 10px', background: mySpeechBubbleColor, lineHeight: 1.6, fontSize: 14, color: 'black' }}>{props.content}</div>
+    </div>
+  </div>
+  )
+}
+
+function SpeechOfOthers(props) {
+  return (
+    <div style={{ overflow: 'auto' }}>
+      <div style={{ float: 'left' }}><img style={{ width: 30, height: 30, borderRadius: 2 }} src="/images/default-avatar.png" /></div>
+      <div className="other-talk-bubble" style={{ float: 'left', maxWidth: '75%', marginLeft: 8, position: 'relative', border: '1px solid white', borderRadius: 4 }}>
+        <div style={{ padding: '6px 10px', background: 'white', lineHeight: 1.6, fontSize: 14, color: 'black' }}>{props.content}</div>
+      </div>
+    </div>
+  )
+}
+
+const contactContainerHeight = 64
+const contactLeftNameFontSize = 14
+const contactAbstractFontSize = 12
+const nameAndAbstractGap = 10
+const contactAvatarSize = 40 
+
+const contactContainerStyle1 = {
+  height: contactContainerHeight,
+  //background: 'blue',
+  borderBottom: '1px solid rgb(239, 239, 239)'
+}
+
+const contactAvatarContainerStyle = {
+  width: contactContainerHeight,
+  height: '100%',
+  padding: (contactContainerHeight - contactAvatarSize) / 2,
+  float: 'left'
+}
+
+const contactRightContainerStyle = {
+  marginLeft: 48,
+  padding: (contactContainerHeight - contactLeftNameFontSize - contactAbstractFontSize - nameAndAbstractGap) / 2 + 'px 0',
+  paddingRight: 10,
+  height: '100%',
+  //background: 'magenta'
+}
+
+const contactNameAndDateContainerStyle = {
+  lineHeight: contactLeftNameFontSize + 'px',
+  height: contactLeftNameFontSize,
+  marginBottom: nameAndAbstractGap
+}
+
+const contactAbstractStyle = {
+  fontSize: contactAbstractFontSize,
+  height: contactAbstractFontSize,
+  lineHeight: contactAbstractFontSize + 'px',
+  overflow: 'hidden',
+  color: 'rgb(177, 177, 177)'
+}
+
+const contactRightContentStyle = {}
+
+function Contact(props) {
+  return (
+    <div style={contactContainerStyle1}>
+      <div style={contactAvatarContainerStyle}>
+        <img style={{ width: contactAvatarSize, height: contactAvatarSize, borderRadius: 2 }} src="/images/default-avatar.png" />
+      </div>
+      <div style={contactRightContainerStyle}>
+        <div style={contactRightContentStyle}>
+          <p style={contactNameAndDateContainerStyle}>
+            <span style={{ fontSize: contactLeftNameFontSize, color: 'black' }}>特工</span>
+            <span style={{ float: 'right', color: 'rgb(177, 177, 177)' }}>10:45 AM</span>
+          </p>
+          <p style={contactAbstractStyle}>一点击就显示已停止工作</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const mainContainerHeight = 500
+const mainContainerWidth = 700
+const topBarHeight = 48
 const rightContainerBackground = 'rgb(243, 243, 243)'
+const contactPercent = 0.35
+
+const mainContainerStyle = {
+  position: 'fixed',
+  right: 0,
+  bottom: 0,
+  width: mainContainerWidth,
+  height: mainContainerHeight,
+  boxShadow: '-3px -2px 8px -1px rgba(0, 0, 0, 0.2)',
+}
 
 const leftContainerStyle = {
-  width: '30%',
+  width: contactPercent * 100 + '%',
   height: '100%',
   background: 'rgb(251, 251, 251)',
   float: 'left',
@@ -12,10 +112,10 @@ const leftContainerStyle = {
 }
 
 const rightContainerStyle = {
-  width: '70%',
+  width: (1 - contactPercent) * 100 + '%',
   height: '100%',
   backgroundColor: rightContainerBackground,
-  marginLeft: '30%'
+  marginLeft: contactPercent * 100 + '%'
 }
 
 const searchContainerStyle = {
@@ -28,45 +128,38 @@ const searchContainerStyle = {
 const searchInputStyle = {
   width: '100%',
   height: '100%',
-  paddingLeft: 10
+  paddingLeft: 10,
+  outline: 0
 }
 
 const contactContainerStyle = {
   width: '100%',
-  height: '100%',
+  height: (mainContainerHeight- topBarHeight) / mainContainerHeight * 100 + '%',
   overflow: 'auto'
 }
 
 const titleContainerStyle = {
   width: '100%',
   height: topBarHeight,
-  padding: '0 6px',
+  padding: '0 20px',
   borderBottom: '1px solid #DDD'
 }
 
 const messageContainerStyle = {
   width: '100%',
-  height: '70%',
+  height: '60%',
+  padding: '12px 20px',
   overflow: 'auto',
   borderBottom: '1px solid #DDD'
 }
 
 const contentContainerStyle = {
-  height: '30%',
+  height: '40%',
   background: 'red'
 }
 
 class Chat extends React.Component {
   render () {
-    const mainContainerStyle = {
-      position: 'fixed',
-      right: 0,
-      bottom: 0,
-      width: 600,
-      height: 400,
-      boxShadow: '-3px -2px 8px -1px rgba(0, 0, 0, 0.2)',
-    }
-
     return (
       <div style={mainContainerStyle}>
         <div style={leftContainerStyle}>
@@ -74,22 +167,27 @@ class Chat extends React.Component {
             <input style={searchInputStyle} type="text" placeholder="查找联系人或群" />
           </div>
           <div style={contactContainerStyle}>
-            <h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1>
-            <h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1>
-            <h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1>
+            <Contact /><Contact /><Contact /><Contact /><Contact />
+            <Contact /><Contact /><Contact /><Contact /><Contact />
+            <Contact /><Contact /><Contact /><Contact /><Contact />
+            <Contact /><Contact /><Contact /><Contact /><Contact />
           </div>
         </div>
         <div style={rightContainerStyle}>
           <div style={titleContainerStyle}>
-            <span style={{ fontSize: 14, lineHeight: topBarHeight + 'px' }}>程序亦非猿</span>
+            <span style={{ fontSize: 16, lineHeight: topBarHeight + 'px', color: 'black' }}>程序亦非猿</span>
           </div>
           <div style={messageContainerStyle}>
-            <h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1>
-            <h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1>
-            <h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1><h1>dsdsd</h1>
-          </div>
+            <ul>
+              <li style={{ marginTop: 20 }}><SpeechOfOthers content="今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的" /></li>
+              <li style={{ marginTop: 20 }}><SpeechOfOthers content="今天你好吗？" /></li>
+              <li style={{ marginTop: 20 }}><SpeechOfMy content="今天你好吗？" /></li>
+              <li style={{ marginTop: 20 }}><SpeechOfOthers content="今天你好吗？" /></li>
+              <li style={{ marginTop: 20 }}><SpeechOfMy content="今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的今天应该去验光的" /></li>
+            </ul>
+      </div>
           <div style={contentContainerStyle}>
-            <textarea style={{ width: '100%', height: '100%', padding: 10, border: 0, background: rightContainerBackground }} />
+            <textarea style={{ width: '100%', height: '100%', padding: 10, border: 0, background: rightContainerBackground, outline: 0 }} />
           </div>
         </div>
       </div>
