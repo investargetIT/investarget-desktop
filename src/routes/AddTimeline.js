609b6a3e9e811495444d1e7c8c2046b357ef5ff6
Peter Xu <xzm920@163.com>
2017-08-01 17:03:30 +0800

    添加：交易师创建时间轴

--- src/routes/AddTimeline.js
+++ src/routes/AddTimeline.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import * as api from '../api'
 import { withRouter } from 'dva/router'
-
+import { getCurrentUser } from '../utils/util'
 import { Button } from 'antd'
 import MainLayout from '../components/MainLayout'
 import PageTitle from '../components/PageTitle'
 import SelectOrganization from '../components/SelectOrganization'
 import SelectUser from '../components/SelectUser'
+import SelectInvestor from '../components/SelectInvestor'
+
 
 
 class AddTimeline extends React.Component {
@@ -16,6 +18,7 @@ class AddTimeline extends React.Component {
     const projId = Number(this.props.location.query.projId)
     this.state = {
       projId: projId,
+      projTitle: '',
       current: 0,
       selectedOrgs: [],
       selectedUsers: [], // 数组项的结构 {investor: ##, trader: ##}
@@ -47,7 +50,7 @@ class AddTimeline extends React.Component {
         return api.addTimeline(params)
       })
     ).then(results => {
-      console.log('>>>', results)
+      this.props.router.replace('/app/timeline/list')
     })
   }
 
@@ -59,39 +62,69 @@ class AddTimeline extends React.Component {
     this.setState({ selectedUsers })
   }
 
+  componentDidMount() {
+    api.getProjLangDetail(this.state.projId).then(result => {
+      const projTitle = result.data.projtitle
+      this.setState({ projTitle })
+    })
+  }
+
   render() {
-    const { current, selectedOrgs, selectedUsers } = this.state
+    const { projTitle, current, selectedOrgs, selectedUsers } = this.state
+    const userId = getCurrentUser()
 
     return (
       <MainLayout location={location}>
         <PageTitle title="创建时间轴" />
         <div>
-          <h3 style={{lineHeight: 2}}>项目名称：</h3>
+          <h3 style={{lineHeight: 2}}>项目名称：{projTitle}</h3>
 
-          <div style={{padding: '8px 0', borderBottom: '1px solid #eee'}}>
-            <p style={{fontSize: '13px'}}>
-              { current == 0 ? <span>1. 选择机构</span> : null }
-              { current == 1 ? <span>2. 选择用户</span> : null }
-            </p>
-          </div>
+          {
+            hasPerm('usersys.as_admin') ? (
+              <div>
+                <div style={{padding: '8px 0', borderBottom: '1px solid #eee'}}>
+                  <p style={{fontSize: '13px'}}>
+                    { current == 0 ? <span>1. 选择机构</span> : null }
+                    { current == 1 ? <span>2. 选择用户</span> : null }
+                  </p>
+                </div>
 
-          <div style={{padding: '16px'}}>
-            { current == 0 ? <SelectOrganization value={selectedOrgs} onChange={this.handleSelectOrg} /> : null }
-            { current == 1 ? <SelectUser value={selectedUsers} onChange={this.handleSelectUser} /> : null }
-          </div>
+                <div style={{padding: '16px'}}>
+                  { current == 0 ? <SelectOrganization value={selectedOrgs} onChange={this.handleSelectOrg} /> : null }
+                  { current == 1 ? <SelectUser selectedOrgs={selectedOrgs} value={selectedUsers} onChange={this.handleSelectUser} /> : null }
+                </div>
 
-          { current == 0 ? (
-            <div style={{textAlign: 'right', padding: '0 16px', marginTop: '-16px'}}>
-              <Button disabled={selectedOrgs.length == 0} type="primary" onClick={this.handleNext}>下一步</Button>
-            </div>
-          ) : null }
+                { current == 0 ? (
+                  <div style={{textAlign: 'right', padding: '0 16px', marginTop: '-16px'}}>
+                    <Button disabled={selectedOrgs.length == 0} type="primary" onClick={this.handleNext}>下一步</Button>
+                  </div>
+                ) : null }
 
-          { current == 1 ? (
-            <div style={{textAlign: 'right', padding: '0 16px', marginTop: '-16px'}}>
-              <Button onClick={ this.handleBack}>返回</Button>
-              <Button disabled={selectedUsers.length == 0} type="primary" onClick={this.handleCreate}>创建</Button>
-            </div>
-          ) : null }
+                { current == 1 ? (
+                  <div style={{textAlign: 'right', padding: '0 16px', marginTop: '-16px'}}>
+                    <Button onClick={ this.handleBack}>返回</Button>
+                    <Button disabled={selectedUsers.length == 0} type="primary" onClick={this.handleCreate}>创建</Button>
+                  </div>
+                ) : null }
+              </div>
+            ) : null
+          }
+
+          {
+            hasPerm('usersys.as_trader') ? (
+              <div>
+                <div style={{padding: '8px 0', borderBottom: '1px solid #eee'}}>
+                  <p style={{fontSize: '13px'}}>选择用户</p>
+                </div>
+                <div style={{padding: '16px'}}>
+                  <SelectInvestor traderId={userId} value={selectedUsers} onChange={this.handleSelectUser} />
+                </div>
+                <div style={{textAlign: 'right', padding: '0 16px', marginTop: '-16px'}}>
+                  <Button disabled={selectedUsers.length == 0} type="primary" onClick={this.handleCreate}>创建</Button>
+                </div>
+              </div>
+            ) : null
+          }
 
         </div>
       </MainLayout>
