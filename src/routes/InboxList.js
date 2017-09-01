4054999c47f5e104b1f013ec8ba38201531d930e
Peter Xu <xzm920@163.com>
2017-09-01 15:47:24 +0800

    修改消息中心的样式

--- src/routes/InboxList.js
+++ src/routes/InboxList.js
@@ -1,208 +1,152 @@
 import React from 'react'
-import { connect } from 'dva'
 import LeftRightLayout from '../components/LeftRightLayout'
-import { Pagination } from 'antd'
+import { Button, Table, Pagination } from 'antd'
 import { getMsg, readMsg } from '../api'
-import { i18n } from '../utils/util'
+import { i18n, handleError } from '../utils/util'
 
-const leftContainerStyle = {
-  width: '50%',
-  minHeight: 360,
-  float: 'left',
-  borderRight: '1px solid rgb(236, 236, 235)'
-}
 
-const rightContainerStyle = {
-  width: '50%',
-  minHeight: 360,
-  marginLeft: '50%',
-}
+const tableStyle = { marginBottom: '24px' }
+const iconStyle = { width: '8px', height: '8px', borderRadius: '50%', backgroundColor: '#108ee9' }
+const backStyle = { fontSize: '13px', marginLeft: '8px' }
+const detailTitleStyle = { textAlign:'center' }
+const detailTimeStyle = { textAlign:'center', color: 'rgba(153, 153, 153, 0.85)' }
+const lineStyle = { margin: '24px 0', height: '1px', backgroundColor: '#eaeaea' }
 
-const headerStyle = {
-  fontSize: 18,
-  lineHeight: 2.4,
-  textAlign: 'center',
-  borderBottom: '1px solid rgb(236, 236, 235)'
-}
 
-const titleStyle = {
-  padding: '14px 20px',
-  fontSize: 13,
-  borderBottom: '1px solid rgb(236, 236, 235)',
-  cursor: 'pointer'
-}
-
-const background = 'rgb(236, 235, 235)'
-
-const activeTitleStyle = { ...titleStyle, background }
-
-const data = [
-  {
-    id: 1,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2017-06-07 15:59\n\n投资人刘璐收藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 2,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 3,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project  to contact the investor as soon as possible.'
-  },
-  {
-    id: 4,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 5,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project Project PIONEER: Breakthrough Technology for Gynecog in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 6,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2017-06-07 15:59\n\n投资人刘璐收藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 7,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 8,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu followed Project Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 9,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu l Project Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-  {
-    id: 10,
-    title: '有投资人收藏了您的项目，New investors have followed your project',
-    detail: '发送时间： 2011-07-07 15:59\n\n投资人刘璐大叔藏了您的先锋项目：妇科癌症的领先治疗技术项目 。请及时登录联系投资人。Investor liu lu fo Project PIONEER: Breakthrough Technology for Gynecological Cancers. Please log in and take the chance to contact the investor as soon as possible.'
-  },
-]
-
-const PAGE_SIZE = 10
 
 class InboxList extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      data: data,
-      currentId: 1,
-      total: data.length,
-      pageIndex: this.props.location.query.page_index || 1
+      showDetail: false,
+      data: [],
+      total: 0,
+      loading: false,
+      pageIndex: this.props.location.query.page_index || 1,
+      pageSize: this.props.location.query.page_size || 10,
+      selectedMsg: [],
+      currentMsg: null,
     }
   }
 
   componentDidMount() {
+    this.getMessageList()
+  }
+
+  handleItemClicked = (record) => {
+    const { id } = record
+    const msg = this.state.data.filter(item => item.id == id)[0]
+    if (msg) {
+      this.setState({ showDetail: true, currentMsg: msg })
+      if (!msg.isRead) {
+        readMsg(id).then(() => {})
+          .catch(error => { handleError(error) })
+      }
+    }
+  }
+
+  // pageIndex, pageSize 变化时，重置 selectedMsg
+
+  handlePageChange = (page) => {
+    this.setState({ pageIndex: page, selectedMsg: [] }, this.getMessageList)
+  }
+
+  handlePageSizeChange = (current, pageSize) => {
+    this.setState({ pageSize, pageIndex: 1, selectedMsg: [] }, this.getMessageList)
+  }
+
+  getMessageList = () => {
+    this.setState({ loading: true })
     const param = {
       page_index: this.state.pageIndex,
-      page_size: PAGE_SIZE
+      page_size: this.state.pageSize,
     }
     getMsg(param).then(data => {
       const list = data.data.data.map(m => {
-        const title = m.messagetitle
-        const detail = m.content
-        return { ...m, title, detail }
+        m.created = m.created.slice(0,19).replace('T', ' ')
+        return m
       })
       this.setState({
         data: list,
         total: data.data.count,
-        currentId: list.length > 0 && list[0].id
+        loading: false,
       })
     })
   }
 
-  handleItemClicked(id) {
-    this.setState({ currentId: id })
-    const index = this.state.data.map(m => m.id).indexOf(id)
-    const msg = this.state.data[index]
-    if (msg.isRead) return
-    readMsg(id).then(data => {
-      msg.isRead = true
-      this.setState({ data: this.state.data })
-    }, error => {
-      this.props.dispatch({
-        type: 'app/findError',
-        payload: error
+  handleReadMsg = () => {
+    const ids = this.state.selectedMsg
+    const q = ids.map(id => readMsg(id))
+    Promise.all(q)
+      .then(() => {
+        this.setState({ selectedMsg: [] })
+        this.getMessageList()
       })
-    })
+      .catch(error => {
+        handleError(error)
+      })
   }
 
-  handlePageIndexChange(newIndex) {
-    const param = {
-      page_index: newIndex,
-      page_size: PAGE_SIZE
-    }
-    getMsg(param).then(data => {
-      const list = data.data.data.map(m => {
-        const title = m.messagetitle
-        const detail = m.content
-        return { ...m, title, detail }
-      })
-      history.pushState('', `?page_index=${newIndex}`)
-      this.setState({
-        data: list,
-        total: data.data.count,
-        pageIndex: newIndex
-      })
-    }, error => {
-      this.props.dispatch({
-        type: 'app/findError',
-        payload: error
-      })
+  handleBackToList = () => {
+    this.setState({
+      showDetail: false,
+      currentMsg: null,
     })
+    this.getMessageList()
+  }
+
+  onSelectChange = (selectedMsg) => {
+    this.setState({ selectedMsg })
   }
 
   render () {
+    const { showDetail, total, data, loading, pageIndex, pageSize, selectedMsg, currentMsg } = this.state
+
+    const columns = [
+      { title: ' ', key: 'isRead', width: 24, render: (text, record) => {
+        return record.isRead ? null : <div style={iconStyle}></div>
+      }},
+      { title: i18n('inbox.title'), key: 'title', dataIndex: 'messagetitle', className: 'pointer', onCellClick: this.handleItemClicked },
+      { title: i18n('inbox.time'), key: 'time', dataIndex: 'created', width: 200 },
+    ]
+    const rowSelection = {
+      selectedRowKeys: selectedMsg,
+      onChange: this.onSelectChange,
+    }
+
     return (
       <LeftRightLayout
-        style={{ background: 'white', overflow: 'auto' }}
-        location={this.props.location}>
+        location={this.props.location}
+        title={showDetail ? (
+            <a href="javascript:void(0)" onClick={this.handleBackToList} style={backStyle}>&lt; {i18n('inbox.back_to_list')}</a>
+          ) : i18n('inbox.message_list')}>
 
-        <div style={leftContainerStyle}>
+        {
+          showDetail ? (
+            <div>
+              <h1 style={detailTitleStyle}>{currentMsg.messagetitle}</h1>
+              <h3 style={detailTimeStyle}>{currentMsg.created}</h3>
+              <div style={lineStyle}></div>
+              <p>{currentMsg.content}</p>
+            </div>
+          ) : (
+            <div>
+              <Table style={tableStyle} rowSelection={rowSelection} columns={columns} dataSource={data} rowKey={record=>record.id} loading={loading} pagination={false} />
 
-          <div style={headerStyle}>{i18n('inbox.message_list')}</div>
-
-          <div><ul>
-            {this.state.data.map(m => <li onClick={this.handleItemClicked.bind(this, m.id)} key={m.id} style={m.id === this.state.currentId ? activeTitleStyle : titleStyle}><p style={{ fontWeight: m.isRead ? 'normal' : 'bold' }}>{m.title}</p></li>)}
-          </ul></div>
-
-          <div style={{ padding: 20, textAlign: 'center' }}>
-            <Pagination
-              total={this.state.total}
-              current={this.state.pageIndex}
-              pageSize={PAGE_SIZE}
-              onChange={this.handlePageIndexChange} />
-          </div>
-
-        </div>
-
-        <div style={rightContainerStyle}>
-
-          <div style={headerStyle}>{i18n('inbox.message_detail')}</div>
-
-          <div style={{ padding: '30px 14px', fontSize: 13, lineHeight: 1.8 }}>
-            { this.state.data.length > 0 ?
-              this.state.data.filter(f => f.id === this.state.currentId)[0].detail
-              .split('\n').map((item, key) => <span key={key}>{item}<br /></span>)
-             : null }
-          </div>
-
-        </div>
+              <div className="clearfix" style={{marginBottom: '24px'}}>
+                <div style={{ float: 'left' }}>
+                  <Button style={{marginLeft: '8px'}} disabled={selectedMsg.length == 0} onClick={this.handleReadMsg}>{i18n('inbox.mark_as_read')}</Button>
+                </div>
+                <Pagination style={{ float: 'right' }} total={total} current={pageIndex} pageSize={pageSize} onChange={this.handlePageChange} showSizeChanger onShowSizeChange={this.handlePageSizeChange} showQuickJumper />
+              </div>
+            </div>
+          )
+        }
 
       </LeftRightLayout>
     )
   }
 }
 
-export default connect()(InboxList)
+export default InboxList
