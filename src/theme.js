78964fdddc7251c0ad058529e8e898fc63e68fb2
Peter Xu <xzm920@163.com>
2017-11-15 13:56:09 +0800

    添加覆盖样式

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,26 @@
+
 var theme = {
-  // "@primary-color": "#1DA57A",
-  "@body-background": "#1d2939",
-  "@font-family": "'LatoRegular', 'Lucida Grande', 'Lucida Sans Unicode', Helvetica, sans-serif !important",
-  "@font-size-base": "14px",
-  "@line-height-base": "21px",
+
+
+  // Colors
+  "primary-color": "#428BCA",
+  "success-color": "#1CAF9A",
+  "warning-color": "#F0AD4E",
+  "error-color": "#D9534F",
+
+  "body-background": "#1d2939",
+  "text-color": "#636e7b",
+  "font-size-base": "14px",
+  "line-height-base": 1.5,
+
+  "border-color-base": "#ccc",
+
+  // Input
+  "input-color": "#555",
+  "input-hover-border-color": "#999",
+
+  // Outline
+  "outline-color": "#999",
 }
 
 module.exports = theme
