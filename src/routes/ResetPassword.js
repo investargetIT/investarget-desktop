118c0888b3a2c563fc1c09a625d38a478af49f3a
Peter Xu <xzm920@163.com>
2017-11-24 19:21:56 +0800

    重设密码按设计稿实现

--- src/routes/ResetPassword.js
+++ src/routes/ResetPassword.js
@@ -9,6 +9,10 @@ import { Mobile, Code, Password, Submit } from '../components/Form'
 import { i18n, handleError } from '../utils/util'
 import * as api from '../api'
 import { ApiError } from '../utils/request'
+import LoginContainer from '../components/LoginContainer'
+import GlobalMobile from '../components/GlobalMobile'
+import FormError from '../utils/FormError'
+import HandleError from '../components/HandleError'
 
 const FormItem = Form.Item
 
@@ -16,6 +20,13 @@ const titleStyle = {
   textAlign: 'center',
   lineHeight: 4
 }
+const formStyle = {width:418,height:400,padding:'0 19px',background:'rgba(47,48,49,.8)',position:'absolute',top:175,right:20,zIndex:1,color:'#fff'}
+const formTitleStyle = {padding:'24px 0 18px',fontSize:22,fontWeight:400,textAlign:'center',color:'#fff',borderBottom:'1px solid #fff'}
+const formSubtitleStyle = {fontSize:16,padding:'12px 16px',fontWeight:200}
+const formInputStyle = {border:'none',fontSize:16,fontWeight:200,color:'#989898',padding:'12px 16px',paddingRight:40,height:50}
+const codeButtonStyle = {width:'100%',height:'50px',border:'none',backgroundColor:'#fff',textAlign:'left',fontSize:16,color:'#656565'}
+const inputStyle = {border:'none',fontSize:16,fontWeight:200,padding:'12px 16px',height:50,marginBottom:8}
+const submitStyle = {width:'100%',height:50,fontSize:20,backgroundColor:'rgba(35,126,205,.8)',border:'none',color:'#fff',fontWeight:200}
 
 
 class ResetPassword extends React.Component {
@@ -46,10 +57,7 @@ class ResetPassword extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const smstoken = this.state.smstoken
-    if (!smstoken) {
-      message.error(i18n('account.require_code'))
-      return
-    }
+
     this.props.form.validateFields((err, values) => {
       if(!err) {
         const { smstoken } = this.state
@@ -66,12 +74,32 @@ class ResetPassword extends React.Component {
           Modal.info({
             title: i18n('account.password_reset_ok'),
             onOk: () => {
+              localStorage.removeItem('login_info')
               this.props.router.push('/login')
             }
           })
         }).catch(error => {
           handleError(error)
         })
+      } else {
+        // 按字段顺序处理错误，只处理第一个错误
+        let fields = ['mobileInfo', 'smstoken', 'code', 'password']
+        for (let i = 0, len = fields.length; i < len; i++) {
+          let field = fields[i]
+          if (field == 'smstoken') {
+            let smstoken = localStorage.getItem('smstoken')
+            if (!smstoken) {
+              Modal.error({ title: i18n('account.require_code') })
+              return
+            }
+          }
+          let errField = err[field]
+          if (errField) {
+            let error = errField.errors[0]
+            handleError(new FormError(error.message))
+            return
+          }
+        }
       }
     })
   }
@@ -138,19 +166,86 @@ class ResetPassword extends React.Component {
   }
 
   render() {
+    const { getFieldDecorator, getFieldValue } = this.props.form
+
+    const codeValue = this.state.fetchSmsCodeValue ? i18n('account.send_wait_time', {'second': this.state.fetchSmsCodeValue}) : null
+
+    function checkMobileInfo(rule, value, callback) {
+      if (value.areaCode == '') {
+        callback(i18n('areacode_not_empty'))
+      // } else if (!allowAreaCode.includes(value.areaCode)) {
+      //   callback(i18n('areacode_invalid'))
+      } else if (value.mobile == '') {
+        callback(i18n('mobile_not_empty'))
+      } else if (!/^\d+$/.test(value.mobile)) {
+        callback(i18n('mobile_incorrect_format'))
+      } else {
+        callback()
+      }
+    }
+
     return (
-      <LeftRightLayout location={this.props.location}>
-          <h2 style={titleStyle}>{i18n('account.reset_password')}</h2>
-          <Form onSubmit={this.handleSubmit}>
-            <Mobile disabled={this.mobile&&this.areaCode ? true : false} required country={this.props.country} mobile={this.mobile} areaCode={this.areaCode}/>
-            <Code
-              loading={this.state.loading}
-              value={this.state.fetchSmsCodeValue ? i18n('account.send_wait_time', {'second': this.state.fetchSmsCodeValue}) : null}
-              onFetchButtonClicked={this.handleFetchButtonClicked} />
-            <Password />
-            <Submit loading={this.props.loading} />
-          </Form>
-      </LeftRightLayout>
+      <LoginContainer>
+        <Form onSubmit={this.handleSubmit} className="it-login-form">
+          <div style={formStyle}>
+            <h1 style={formTitleStyle}>重置密码</h1>
+            <p style={formSubtitleStyle}>输入手机号码，重置密码！</p>
+
+            <div style={{marginBottom: 8}}>
+              {getFieldDecorator('mobileInfo', {
+                rules: [{ required: true }, { type: 'object' }, { validator: checkMobileInfo }],
+                initialValue: { areaCode: this.areaCode || '86', mobile: this.mobile || '' },
+              })(
+                <GlobalMobile disabled={this.mobile&&this.areaCode?true:false} onBlur={this.handleMobileBlur} />
+              )}
+            </div>
+
+            <div style={{marginBottom: 8}}>
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("code", {
+                    rules: [{
+                      required: true, message: '请输入验证码',
+                    }],
+                  })(<Input style={formInputStyle} placeholder="请输入验证码" />)}
+                </Col>
+                <Col span={12}>
+                  <Button
+                    loading={this.state.loading}
+                    disabled={codeValue ? true : false}
+                    onClick={this.handleFetchButtonClicked.bind(this)}
+                    size="large"
+                    style={codeButtonStyle}
+                  >
+                    {this.state.loading
+                      ? i18n("account.is_fetching_code")
+                      : (codeValue ? <span style={{color:'#237ccc'}}>{codeValue}</span>
+                                   : (<span>
+                                        <span style={{textDecoration:'underline'}}>{i18n("account.fetch_code")}</span>
+                                        &nbsp;<span style={{color:'#237ccc'}}>(60s)</span>
+                                    </span>)
+                      )
+                    }
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+
+            {getFieldDecorator('password', { rules: [{required: true, message: '请输入新的密码'}] })(
+              <Input style={inputStyle} placeholder="请输入新的密码" type="password" />
+            )}
+
+            <Button htmlType="submit" style={submitStyle} loading={this.props.loading}>提交</Button>
+
+            <div style={{padding:8,paddingLeft:16}}>
+              已有账号？<Link to="/login" style={{textDecoration:'underline'}}>立即登录</Link>
+            </div>
+
+          </div>
+        </Form>
+
+        <HandleError pathname={encodeURIComponent(this.props.location.pathname + this.props.location.search)} />
+      </LoginContainer>
     )
   }
 }
