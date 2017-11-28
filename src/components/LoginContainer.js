6650d2576a6969b5c092042bd1f8852b41db98b9
Oliver <olimovier@gmail.com>
2017-11-28 10:36:27 +0800

    登录页面根据data source设置背景图logo

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,17 +1,34 @@
-
 const containerStyle = {background:'#fff'}
 const headerStyle = {width: 1200, height: 80, margin: '0 auto', backgroundColor: '#fff'}
 const logoStyle = {height: 70, padding: '10px 0'}
-const bodyWrapStyle = {margin: '0 auto', minWidth: 1200,maxWidth: 1920, height: 750, backgroundImage: 'url(/images/background.jpg)', backgroundPosition: 'center center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}
 const bodyStyle = {width: 1200, height: '100%', margin: '0 auto', position: 'relative'}
 const footerStyle = {width: 1200, height: 100, margin: '0 auto', backgroundColor: '#fff'}
 const copyrightStyle = {textAlign:'center',height:20,lineHeight:'20px',paddingTop:40,fontSize:16,color:'#989898'}
 
 function LoginContainer(props) {
+  const source = parseInt(localStorage.getItem('source'), 10)
+  let logo, bg;
+  if (source === 1) {
+    logo = '/images/logo.jpg';
+    bg = 'url(/images/background.jpg)';
+  } else if (source === 2) {
+    logo = '/images/autospace.png';
+    bg = 'url(/images/bg_autospace.png)';
+  }
+  const bodyWrapStyle = {
+    margin: '0 auto', 
+    minWidth: 1200, 
+    maxWidth: 1920, 
+    height: 750, 
+    backgroundImage: bg, 
+    backgroundPosition: 'center center', 
+    backgroundSize: 'cover', 
+    backgroundRepeat: 'no-repeat'
+  };
   return (
     <div style={containerStyle}>
       <div style={headerStyle}>
-        <img src="/images/logo.jpg" style={logoStyle} />
+        <img src={logo} style={logoStyle} />
       </div>
       <div style={bodyWrapStyle}>
         <div style={bodyStyle}>
