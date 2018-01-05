b65671804cde0042d7d8673d6ce5102abd182c46
Oliver <olimovier@gmail.com>
2018-01-05 18:14:19 +0800

    删除修改项目附件上传的无用的代码

--- src/components/ProjectAttachments.js
+++ src/components/ProjectAttachments.js
@@ -145,36 +145,13 @@ class ProjectAttachments extends React.Component {
 
   }
 
-  handleFileChange = ({ file, fileList, event }) => {
-    console.log('>>>', file, fileList, event);
-    if (file.status === 'uploading') {
-      this.handleFileUpload(file)
-    } else if (file.status === 'done') {
+  handleFileChange = ({ file }) => {
+    if (file.status === 'done') {
       this.handleFileUploadDone(file)
-    } else if (file.status === 'error') {
-      this.handleFileUploadError(file)
-    } else if (file.status === 'removed') {
-      this.handleFileRemove(file)
-    }
-  }
-
-  handleFileUpload = (file) => {
-    if (!file.filetype) {
-      file.filetype = this.state.activeDir
-      file.filename = file.name
-      this.setState({ fileList: [ ...this.state.fileList, file] })
-    } else {
-      const index  = _.findIndex(this.state.fileList, function(item) {
-        return item.filetype == file.filetype && item.filename == file.filename
-      })
-      this.setState({
-        fileList: [ ...this.state.fileList.slice(0, index), file, ...this.state.fileList.slice(index+1) ]
-      })
-    }
+    } 
   }
 
   handleFileUploadDone = (file) => {
-    this.setState({ spinning: false });
 
     file.bucket = 'file'
     file.key = file.response.result.key
@@ -193,33 +170,15 @@ class ProjectAttachments extends React.Component {
     })
   }
 
-  handleFileUploadError = (file) => {
-    this.setState({ spinning: false });
-    const index  = _.findIndex(this.state.fileList, function(item) {
-      return item.filetype == file.filetype && item.filename == file.filename
-    })
-    this.setState({
-      fileList: [ ...this.state.fileList.slice(0, index), file, ...this.state.fileList.slice(index+1) ]
-    })
-  }
-
   handleFileRemove = (file) => {
     this.setState({ spinning: true });
-    this.removeAttachment(file).then(result => {
-      const index  = _.findIndex(this.state.fileList, function(item) {
-        return item.filetype == file.filetype && item.filename == file.filename
-      })
-      this.setState({
-        fileList: [ ...this.state.fileList.slice(0, index), ...this.state.fileList.slice(index+1) ],
-        spinning: false
-      })
-    }, error => {
-      this.handleError(error)
-    })
+    this.removeAttachment(file)
+      .then(this.getAttachment)
+      .catch(this.handleError);
   }
 
-
   getAttachment = () => {
+    this.setState({ spinning: true });
     const projId = this.props.projId
     const param = {
       proj: projId,
@@ -243,6 +202,7 @@ class ProjectAttachments extends React.Component {
       this.setState({
         fileList,
         dirs: [ ...this.state.dirs, ...otherDirs ],
+        spinning: false, 
       })
     })
     .catch(error => {
@@ -254,9 +214,9 @@ class ProjectAttachments extends React.Component {
     const projId = this.props.projId
     const data = {
       proj: this.props.projId,
-      filetype: file.filetype,
+      filetype: this.state.activeDir, 
       bucket: file.bucket,
-      filename: file.filename,
+      filename: file.name,
       key: file.key,
     }
     return api.addProjAttachment(data).then(result => {
@@ -388,13 +348,11 @@ class ProjectAttachments extends React.Component {
     const { fileList, dirs } = this.state
 
     let panes = dirs.map(item => ({ title: item, key: item, closable: !fixedDirs.includes(item) }))
-    let targetFileList = fileList.filter(item => item.filetype == this.state.activeDir)
     let doneFileList = fileList.filter(file => file.status == 'done')
 
     const uploadProps = {
       action: baseUrl + "/service/qiniubigupload?bucket=file",
       accept: fileExtensions.join(','),
-      fileList: targetFileList,
       onChange: this.handleFileChange,
       onRemove: this.handleFileRemoveConfirm,
       showUploadList: false
@@ -403,43 +361,6 @@ class ProjectAttachments extends React.Component {
     return (
       <div style={{ paddingTop: 15 }}>
 
-        {/* <div style={{ marginBottom: 16 }}>
-          <Input
-            value={this.state.newDir}
-            onChange={(e) => {this.setState({ newDir: e.target.value })}}
-            style={{width: 'auto', marginRight: '8px'}}
-            placeholder={i18n('project.add_directory')}
-          />
-          <Button onClick={this.addDir} disabled={this.state.newDir == ''}>ADD</Button>
-        </div>
-
-        <div style={{ height: 20 }} />
-        <Tabs
-          hideAdd
-          onChange={(activeKey) => {this.setState({activeDir: activeKey})}}
-          activeKey={this.state.activeDir}
-          type="editable-card"
-          onEdit={this.handleTabsEdit}
-        >
-          {panes.map(pane => <TabPane tab={pane.title} key={pane.key} closable={pane.closable}></TabPane>)}
-        </Tabs>
-
-        <Dragger
-          action={baseUrl + "/service/qiniubigupload?bucket=file"}
-          accept={fileExtensions.join(',')}
-          beforeUpload={this.beforeUpload}
-          fileList={targetFileList}
-          onChange={this.handleFileChange}
-          onRemove={this.handleFileRemoveConfirm}
-        >
-          <p className="ant-upload-drag-icon">
-            <Icon type="inbox" />
-          </p>
-          <p className="ant-upload-text">{i18n('project.click_drag_upload')}</p>
-          <p className="ant-upload-hint">pdf, doc, docx, xls, xlsx, ppt, pptx</p>
-        </Dragger>
-        <Button onClick={this.handleMobileUploadBtnClicked} style={{ margin: '20px 0' }}>手机上传</Button> */}
-        
         <Row style={{ backgroundColor: '#ebf0f3', padding: '0 20px' }}>
           <Col span={16}>
             <div style={{ height: 40, display: 'flex', alignItems: 'center', color: '#282828', fontSize: 14  }}>目录</div>
