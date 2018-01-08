ac20b0005ce6e449f783d7311056ef6a9b9b2ea0
Oliver <olimovier@gmail.com>
2018-01-08 17:54:06 +0800

    新增权限组命名

--- src/constants.js
+++ src/constants.js
@@ -92,6 +92,7 @@ export const CONTENT_TYPE_ID_TO_PERM_GROUP = baseUrl === prodUrl ?
   70: '机构BD', 
   67: '用户信息修改日志管理', 
   69: '邮件管理', 
+  6: '用户备注管理', 
 } 
 : 
 {
@@ -112,6 +113,7 @@ export const CONTENT_TYPE_ID_TO_PERM_GROUP = baseUrl === prodUrl ?
   78: '机构BD', 
   67: '用户信息修改日志管理', 
   63: '邮件管理', 
+  69: '用户备注管理', 
 }
 
 export const KEY_TO_ICON = {
