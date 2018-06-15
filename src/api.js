3b756a55e14d92dba89ea2c6b9defc0d69326488
Oliver <olimovier@gmail.com>
2018-06-15 22:03:25 +0800

    着手开发用户详情上传附件功能

--- src/api.js
+++ src/api.js
@@ -510,6 +510,8 @@ export function deleteUserRemark(id) {
 
 export const getUserAttachment = params => r('/user/atta/?' + qs.stringify(params));
 export const deleteUserAttachment = id => r('/user/atta/' + id + '/', 'DELETE');
+export const addUserAttachment = body => r('/user/atta/', 'POST', body);
+
 export const getUserInvestEvent = params => r('/user/event/?' + qs.stringify(params));
 export const addUserInvestEvent = body => r('/user/event/', 'POST', body);
 export const deleteUserInvestEvent = id => r('/user/event/' + id + '/', 'DELETE');
