16eb7c6ab9d3426712ea1da798c9ef810d2cef98
Oliver <olimovier@gmail.com>
2018-07-25 11:18:46 +0800

    机构BD添加新投资人后建立联系

--- src/components/ModalAddUser.js
+++ src/components/ModalAddUser.js
@@ -5,7 +5,10 @@ import {
 } from 'antd';
 import SimpleUserForm from './SimpleUserForm';
 import * as api from '../api';
-import { handleError } from '../utils/util';
+import { 
+  handleError,
+  isLogin,
+} from '../utils/util';
 
 const AddUserForm = Form.create()(SimpleUserForm);
 
@@ -25,15 +28,26 @@ class ModalAddUser extends React.Component {
     this.addForm.validateFields((err, values) => {
       if (!err) {
         this.setState({ isLoading: true });
-        values.userstatus = 1; // 默认待审核
-        values.registersource = 3; // 标记注册来源
-        values.org = this.props.org.id;
-        api.addUser(values)
+        this.addUserAndRelation(values)
           .then(this.props.onCancel)
-          .catch(handleError)
+          .catch(handleError) // 可能会添加一个已经存在的投资人所以要捕获这个错误
           .finally(() => this.setState({ isLoading: false }));
       }
     })
+  }
+
+  addUserAndRelation = async values => {
+    values.userstatus = 1; // 默认待审核
+    values.registersource = 3; // 标记注册来源
+    values.org = this.props.org.id;
+    const user = await api.addUser(values);
+    const investoruserid = user.data.id;
+    const body = {
+      relationtype: true,
+      investoruser: investoruserid,
+      traderuser: isLogin().id
+    };
+    await api.addUserRelation(body);
   }
 
   render() {
