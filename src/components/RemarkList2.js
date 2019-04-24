f2c13aa01827cc6ad8641b104c26f58128bf08c9
InvesTarget <system@investarget.com>
2019-04-24 13:16:12 +0800

    Remove add button of project library remark

--- src/components/RemarkList2.js
+++ src/components/RemarkList2.js
@@ -99,7 +99,10 @@ class RemarkList extends React.Component {
     const {visible}=this.state
     return (
       <div>
-        <h3 style={remarkTitleStyle}>{i18n('remark.comments')}<Icon type={visible?"minus":"plus"} style={addIconStyle} onClick={this.toggleNewComment} /></h3>
+        <h3 style={remarkTitleStyle}>
+          {i18n('remark.comments')}
+          {this.props.type !== 'library' && <Icon type={visible ? 'minus' : 'plus'} style={addIconStyle} onClick={this.toggleNewComment} />}
+        </h3>
 
         <div style={{display: this.state.visible ? 'block' : 'none'}}>
           <Input.TextArea style={textareaStyle} placeholder={i18n('common.write_comment')} autosize={{maxRows: 6 }} value={this.state.comments} onChange={this.handleChange} />
