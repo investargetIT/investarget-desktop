65a290915223ca05eae20b18d772bbe9bd469828
InvesTarget <system@investarget.com>
2019-12-12 13:12:31 +0800

    Fix an issue

--- src/routes/DataRoom.js
+++ src/routes/DataRoom.js
@@ -101,6 +101,7 @@ class DataRoom extends React.Component {
     this.setState({
       dataRoomTemp,
       selectedDataroomTemp: dataRoomTemp.length > 0 ? '' + dataRoomTemp[0].id : '',
+      pdfPassword: dataRoomTemp.length > 0 ? dataRoomTemp[0].password : '',
       pdfPasswordForTemp: dataRoomTemp.length > 0 ? dataRoomTemp[0].password : '',
     });
   }
