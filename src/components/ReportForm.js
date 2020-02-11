91ab051294cbd05227bbaacd9e394b3d45d0684f
InvesTarget <system@investarget.com>
2020-02-11 19:11:45 +0800

    Update report form

--- src/components/ReportForm.js
+++ src/components/ReportForm.js
@@ -29,7 +29,23 @@ import {
 
 const paraStyle = {lineHeight: 2, marginBottom: '8px'}
 
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 2 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 14 },
+  },
+}
 
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 14, offset: 6 },
+  }
+}
 
 class ProjectBaseForm extends React.Component {
 
@@ -54,34 +70,56 @@ class ProjectBaseForm extends React.Component {
     const { getFieldDecorator } = this.props.form
     return (
       <Form>
-        <div style={{ border: ' 1px solid #CCCCCC' }}>
+        <div style={{ border: '1px solid #CCCCCC' }}>
           <div style={{ backgroundColor: '#F8F8F8' }}>进行中项目工作汇报</div>
-          
-          <div>
-            <BasicFormItem label={i18n('schedule.project')} name="proj" valueType="number">
-              <SelectExistProject />
-            </BasicFormItem>
 
-            <div>
-              <div>本周工作</div>
-              <BasicFormItem label="机构" name="org" >
-                <SelectExistOrganization allowCreate formName="userform" />
-              </BasicFormItem>
-              <BasicFormItem label="投资人" name="bduser" valueType="number">
-                <SelectExistUser />
+          <div style={{ display: 'flex' }}>
+
+            <div style={{ width: 200 }}>
+              <BasicFormItem label={i18n('schedule.project')} name="proj" valueType="number">
+                <SelectExistProject />
               </BasicFormItem>
             </div>
 
-            <div>
-              <div>下周计划</div>
-              {/* <BasicFormItem label={i18n('project.project_english_name')} name="projtitleE" whitespace>
-                <Input />
-              </BasicFormItem> */}
+            <div style={{ flex: 1 }}>
+              <div>
+                <div>本周工作</div>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ flex: 1 }}>
+                    <div style={{ display: 'flex' }}>
+                      <div>机构：</div>
+                      <div style={{ flex: 1 }}>
+                        <BasicFormItem label="" name="org" layout="formItemLayoutWithOutLabel" >
+                          <SelectExistOrganization allowCreate formName="userform" />
+                        </BasicFormItem>
+                      </div>
+                    </div>
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <BasicFormItem label="投资人" name="bduser" valueType="number">
+                      <SelectExistUser />
+                    </BasicFormItem>
+                  </div>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ flex: 1 }}>
+                    <BasicFormItem label="其他" name="others" layout={formItemLayout}>
+                      <Input.TextArea />
+                    </BasicFormItem>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div>下周计划</div>
+                <BasicFormItem label="下周计划" name="next_plan" whitespace>
+                  <Input.TextArea />
+                </BasicFormItem>
+              </div>
             </div>
           </div>
 
         </div>
-
       </Form>
     )
   }
