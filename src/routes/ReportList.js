825823df689b9b9a5f2a5743832369da0485cdcc
InvesTarget <system@investarget.com>
2020-02-16 10:52:21 +0800

    Almost complete report list

--- src/routes/ReportList.js
+++ src/routes/ReportList.js
@@ -3,6 +3,9 @@ import LeftRightLayout from '../components/LeftRightLayout';
 import * as api from '../api';
 import { getUserInfo } from '../utils/util';
 import { connect } from 'dva';
+import { Icon, Table, Pagination } from 'antd';
+import { PAGE_SIZE_OPTIONS } from '../constants';
+import { Link } from 'dva/router';
 
 class ReportList extends React.Component {
 
@@ -39,11 +42,60 @@ class ReportList extends React.Component {
     })
   }
 
+  handlePageChange = (page) => {
+    this.setState({ page }, this.getReportList);
+  }
+
+  handlePageSizeChange = (current, pageSize) => {
+    this.setState({ pageSize, page: 1 }, this.getReportList);
+  }
+
   render() {
     const { location } = this.props;
-    return  (
-    <LeftRightLayout location={location} title="工作报告列表">
-    </LeftRightLayout>
+    const { total, list, loading, page, pageSize } = this.state;
+    const columns = [
+      { title: '姓名', key: 'user', dataIndex: 'user.username' },
+      { title: '开始时间', key: 'startTime', dataIndex: 'startTime', render: text => text.slice(0, 10) },
+      { title: '截止时间', key: 'endTime', dataIndex: 'endTime', render: text => text.slice(0, 10) },
+      {
+        title: '操作', key: 'operation', render: (text, record) => {
+          return (
+            <div>
+              <Link>
+                <Icon type="eye-o" style={{ fontSize: '16px' }} />
+              </Link>
+              <Link style={{ margin: '0 10px' }}>
+                <Icon type="edit" style={{ fontSize: '16px' }} />
+              </Link>
+              <Link>
+                <Icon type="delete" style={{ fontSize: '16px' }} />
+              </Link>
+            </div>
+          );
+        }
+      },
+    ]
+    return (
+      <LeftRightLayout location={location} title="工作报告列表">
+        <Table
+          columns={columns}
+          dataSource={list}
+          rowKey={record => record.id}
+          loading={loading}
+          pagination={false} />
+
+        <Pagination
+          className="ant-table-pagination"
+          total={total}
+          current={page}
+          pageSize={pageSize}
+          onChange={this.handlePageChange}
+          showSizeChanger
+          onShowSizeChange={this.handlePageSizeChange}
+          showQuickJumper
+          pageSizeOptions={PAGE_SIZE_OPTIONS}
+        />
+      </LeftRightLayout>
     );
   }
 }
