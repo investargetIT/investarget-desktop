1080bb6cfd5241de471f71a430e25649e5c1fe89
InvesTarget <system@investarget.com>
2020-02-19 14:31:23 +0800

    Fix add remove text project

--- src/components/ReportForm.js
+++ src/components/ReportForm.js
@@ -361,7 +361,7 @@ class ReportForm extends React.Component {
         <div style={{ display: 'flex', alignItems: 'center' }}>
 
           <div style={{ width: 200 }}>
-            <BasicFormItem name={`text_project_${i}`} layout>
+            <BasicFormItem name={`text_project_${m}`} layout>
               <Input placeholder="项目名称" />
             </BasicFormItem>
           </div>
@@ -379,7 +379,7 @@ class ReportForm extends React.Component {
                     <div style={{ width: 10, marginLeft: 20, marginRight: 10 }}>•</div>
                     <div>其他：</div>
                     <div style={{ flex: 1 }}>
-                      <BasicFormItem name={`newproject_${i}_thisplan`} layout>
+                      <BasicFormItem name={`newproject_${m}_thisplan`} layout>
                         <Input.TextArea autosize={{ minRows: 4 }} placeholder="本周其他与项目相关的工作" />
                       </BasicFormItem>
                     </div>
@@ -392,7 +392,7 @@ class ReportForm extends React.Component {
             <div>
               <div style={{ color: 'black', textDecoration: 'underline', fontWeight: 'bold', lineHeight: 3 }}>下周计划</div>
               <div style={{ marginLeft: 82 }}>
-                <BasicFormItem name={`newproject_${i}_nextplan`} layout>
+                <BasicFormItem name={`newproject_${m}_nextplan`} layout>
                   <Input.TextArea autosize={{ minRows: 4 }} placeholder="下周与项目相关的工作计划" />
                 </BasicFormItem>
               </div>
