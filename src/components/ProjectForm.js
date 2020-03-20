3896a5794a0f2a2c92a299a501c76e875dfa821b
InvesTarget <system@investarget.com>
2020-03-20 08:59:25 +0800

    承揽承做不能是同一个人

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -206,6 +206,7 @@ class ProjectConnectForm extends React.Component {
   }
 
   render() {
+    const { getFieldValue } = this.props.form;
     return (
       <Form>
         {
@@ -240,7 +241,7 @@ class ProjectConnectForm extends React.Component {
         {hasPerm('proj.admin_changeproj') ?
           <BasicFormItem label={i18n('project.take_user')} name="takeUser" valueType="array">
             {/* <SelectAllUser type="trader" /> */}
-            <SelectTrader mode="multiple" />
+            <SelectTrader mode="multiple" disabledOption={getFieldValue('makeUser')} />
           </BasicFormItem>
           :
           <BasicFormItem label={i18n('project.take_user')} name="takeUserName">
@@ -251,7 +252,7 @@ class ProjectConnectForm extends React.Component {
         {hasPerm('proj.admin_changeproj') ?
           <BasicFormItem label={i18n('project.make_user')} name="makeUser" valueType="array">
             {/* <SelectAllUser type="trader" /> */}
-            <SelectTrader mode="multiple" />
+            <SelectTrader mode="multiple" disabledOption={getFieldValue('takeUser')} />
           </BasicFormItem>
           :
           <BasicFormItem label={i18n('project.make_user')} name="makeUserName">
