d71af73455e4996533bb8b9b85f69e9e4a8d76b0
InvesTarget <system@investarget.com>
2020-09-12 16:57:59 +0800

    Update custom annotation layer classname

--- public/pdf_viewer/annotation.js
+++ public/pdf_viewer/annotation.js
@@ -6,13 +6,13 @@ const drawAnnotationLayer = function (page) {
 
   const pageHtml = document.querySelector(`.page[data-page-number="${pageNumber}"]`);
   const svgLayer = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svgLayer.setAttribute('class', 'customAnnotationLayer');
+  svgLayer.setAttribute('class', 'custom-annotation-layer');
   pageHtml.appendChild(svgLayer);
   
   PDFJSAnnotate.setStoreAdapter(new PDFJSAnnotate.LocalStoreAdapter());
   const adapter = PDFJSAnnotate.getStoreAdapter();
   adapter.getAnnotations('test.pdf', pageNumber ).then(annotations => {
-    const svg = document.querySelector(`.page[data-page-number="${pageNumber}"] .customAnnotationLayer`);
+    const svg = document.querySelector(`.page[data-page-number="${pageNumber}"] .custom-annotation-layer`);
     svg.setAttribute('width', viewport.width);
     svg.setAttribute('height', viewport.height);
     PDFJSAnnotate.render(svg, viewport, annotations);
