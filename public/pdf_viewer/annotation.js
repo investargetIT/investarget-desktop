8441235e1fb804e75b66e4d192057e3a1280b477
InvesTarget <system@investarget.com>
2020-09-12 17:02:13 +0800

    Enable rectangle annontation

--- public/pdf_viewer/annotation.js
+++ public/pdf_viewer/annotation.js
@@ -1,12 +1,15 @@
 const { default: PDFJSAnnotate } = PDFAnnotate;
 
+const { UI, config: { annotationLayerName } } = PDFJSAnnotate;
+UI.enableRect('area');
+
 const drawAnnotationLayer = function (page) {
   const { source, pageNumber } = page;
   const { viewport } = source;
 
   const pageHtml = document.querySelector(`.page[data-page-number="${pageNumber}"]`);
   const svgLayer = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svgLayer.setAttribute('class', 'custom-annotation-layer');
+  svgLayer.setAttribute('class', `${annotationLayerName} custom-annotation-layer`);
   pageHtml.appendChild(svgLayer);
   
   PDFJSAnnotate.setStoreAdapter(new PDFJSAnnotate.LocalStoreAdapter());
