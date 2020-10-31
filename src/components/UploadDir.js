839c38bddc26ad76dc67ddf560f9b2bc9d83665d
InvesTarget <system@investarget.com>
2020-10-31 23:22:18 +0800

    Enable directory upload

--- src/components/UploadDir.js
+++ src/components/UploadDir.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 class UploadDir extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.inputElement.directory = true;
+    this.inputElement.webkitdirectory = true;
+  }
+
+  handleClick() {
+    this.inputElement.click();
+  }
+
   render() {
     return (
-      <span className="">
+      <span className="" onClick={this.handleClick}>
         <div className="ant-upload ant-upload-select ant-upload-select-text">
           <span tabIndex="0" className="ant-upload" role="button">
-            <input type="file" accept multiple style={{ display: 'none' }} />
+            <input
+              ref={input => this.inputElement = input}
+              type="file"
+              style={{ display: 'none' }}
+            />
             {this.props.children}
           </span>
         </div>
