6eee2559ef55afa1f6e69ec478ab2b2ffd83eb8e
InvesTarget <system@investarget.com>
2021-02-22 11:49:46 +0800

    Make router work

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Router, Route, Redirect } from 'dva/router'
-import Home from './routes/Home'
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
+import Home from './routes/Home';
+import Login from './components/Login.js';
 // import IndexPage from './routes/IndexPage'
-// import Login from './components/Login.js'
 // import UserList from "./routes/UserList.js"
 // import OrgUserList from './routes/OrgUserList.js'
 // import Register from './routes/Register'
@@ -82,82 +82,84 @@ import Home from './routes/Home'
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={Home} />
-      {/* <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/register1" component={Register1} />
-      <Route path="/recommend-friends" component={RecommendFriends} />
-      <Route path="/recommend-projects" component={RecommendProjects} />
-      <Route path="/app" component={IndexPage} />
-      <Route path="/403" component={AccessDenied} />
-      <Route path="/app/orguser/list" component={OrgUserList} />
-      <Route path={URI_6} component={UserList} />
-      <Route path="/app/user/add" component={AddUser} />
-      <Route path="/app/organization/list" component={OrganizationList} />
-      <Route path="/app/organization/add" component={AddOrganization} />
-      <Route path="/app/organization/selectuser" component={SelectUserToPosition} />
-      <Route path="/app/organization/edit/:id" component={EditOrganization} />
-      <Route path="/app/organization/:id" component={OrgDetail} />
-      <Route path={URI_11} component={LogList} />
-      <Route path={URI_9} component={ModifyPwd} />
-      <Route path={URI_10} component={BasicInfo} />
-      <Route path="/app/projects/library" component={ProjectLibrary} />
-      <Route path="/app/projects/library/:id" component={ProjectLibraryItem} />
-      <Route path="/app/projects/bd" component={ProjectBDList} />
-      <Route path="/app/projects/bd/add" component={AddProjectBD} />
-      <Route path="/app/projects/bd/edit/:id" component={EditProjectBD} />
-      <Route path="/app/projects/list" component={ProjectList} />
-      <Route path="/app/projects/list/recommend" component={ProjectListRecommend} />
-      <Route path="/app/projects/list/favor" component={ProjectListFavor} />
-      <Route path="/app/projects/list/interest" component={ProjectListInterest} />
-      <Route path="/app/projects/published" component={ProjectListPublished} />
-      <Route path="/app/projects/add" component={AddProject} />
-      <Route path="/app/projects/edit/:id" component={EditProject} />
-      <Route path="/app/projects/recommend/:id" component={RecommendProject} />
-      <Route path="/app/projects/:id" component={ProjectDetail} />
-      <Route path={URI_14} component={PermList} />
-      <Route path="/app/dataroom/project/list" component={DataRoomList} />
-      <Route path="/app/dataroom/company/list" component={CompanyDataRoomList} />
-      <Route path="/app/dataroom/detail" component={DataRoom} />
-      <Route path="/app/dataroom/company/detail" component={CompanyDataRoomDetail} />
-      <Route path="/app/timeline/list" component={OrgBdTableList} />
-      <Route path="/app/email/list" component={EmailList} />
-      <Route path="/app/email/detail/:id" component={EmailDetail} />
-      <Route path="/app/user/edit/:id" component={EditUser} />
-      <Route path="/app/user/:id" component={UserDetail} />
-      <Route path="/app/timeline/add" component={AddTimeline} />
-      <Route path="/app/timeline/edit/:id" component={EditTimeline} />
-      <Route path="/app/timeline/:id" component={TimelineDetail} />
-      <Route path={URI_8} component={InboxList} />
-      <Route path="/app/dataroom/add" component={AddDataRoom} />
-      <Route path="/app/marketplace/add" component={AddMarketPlace} />
-      <Route path="/app/marketplace/edit/:id" component={EditMarketPlace} />
-      <Route path="/app/marketplace/:id" component={MarketPlaceDetail} />
-      <Route path={URI_12} component={MyInvestor} />
-      <Route path={URI_13} component={MyTrader} />
-      <Route path="/app/trader/add" component={SelectTraderToRelation} />
-      <Route path="/app/agreement" component={Agreement} />
-      <Route path="/app/wxmsg" component={WxMessage} />
-      <Route path="/app/schedule" component={Schedule} />
-      <Route path="/app/schedule/list" component={ScheduleList} />
-      <Route path="/password" component={ResetPassword} />
-      <Route path="/app/org/bd" component={OrgBDList} />
-      <Route path="/app/org/newbd" component={NewOrgBDNext} />
-      <Route path="/app/orgbd/add" component={NewOrgBD} />
-      <Route path="/app/trader/:id" component={TraderDetail} />
-      <Route path="/app/meeting/bd" component={MeetingBDList} />
-      <Route path="/app/meetingbd/add" component={AddMeetingBD} />
-      <Route path="/app/org/export" component={OrgExportList} />
-      <Route path="/app/orgbd/project/list" component={OrgBDProjList} />
-      <Route path="/app/report/add" component={AddReport} />
-      <Route path="/app/report/list" component={ReportList} />
-      <Route path="/app/report/:id" component={ReportDetail} />
-      <Route path="/app/report/edit/:id" component={EditReport} />
-      <Route path="/app/okr/add" component={AddOKR} />
-      <Route path="/app/okr/list" component={OKRList} />
-      <Route path="/app/okr/edit/:id" component={EditOKR} />
-      <Route path="/app/online-test" component={InternOnlineTest} />
-      <Route path="/app/project-report" component={ProjectReport} /> */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        {/* <Route path="/register" component={Register} />
+        <Route path="/register1" component={Register1} />
+        <Route path="/recommend-friends" component={RecommendFriends} />
+        <Route path="/recommend-projects" component={RecommendProjects} />
+        <Route path="/app" component={IndexPage} />
+        <Route path="/403" component={AccessDenied} />
+        <Route path="/app/orguser/list" component={OrgUserList} />
+        <Route path={URI_6} component={UserList} />
+        <Route path="/app/user/add" component={AddUser} />
+        <Route path="/app/organization/list" component={OrganizationList} />
+        <Route path="/app/organization/add" component={AddOrganization} />
+        <Route path="/app/organization/selectuser" component={SelectUserToPosition} />
+        <Route path="/app/organization/edit/:id" component={EditOrganization} />
+        <Route path="/app/organization/:id" component={OrgDetail} />
+        <Route path={URI_11} component={LogList} />
+        <Route path={URI_9} component={ModifyPwd} />
+        <Route path={URI_10} component={BasicInfo} />
+        <Route path="/app/projects/library" component={ProjectLibrary} />
+        <Route path="/app/projects/library/:id" component={ProjectLibraryItem} />
+        <Route path="/app/projects/bd" component={ProjectBDList} />
+        <Route path="/app/projects/bd/add" component={AddProjectBD} />
+        <Route path="/app/projects/bd/edit/:id" component={EditProjectBD} />
+        <Route path="/app/projects/list" component={ProjectList} />
+        <Route path="/app/projects/list/recommend" component={ProjectListRecommend} />
+        <Route path="/app/projects/list/favor" component={ProjectListFavor} />
+        <Route path="/app/projects/list/interest" component={ProjectListInterest} />
+        <Route path="/app/projects/published" component={ProjectListPublished} />
+        <Route path="/app/projects/add" component={AddProject} />
+        <Route path="/app/projects/edit/:id" component={EditProject} />
+        <Route path="/app/projects/recommend/:id" component={RecommendProject} />
+        <Route path="/app/projects/:id" component={ProjectDetail} />
+        <Route path={URI_14} component={PermList} />
+        <Route path="/app/dataroom/project/list" component={DataRoomList} />
+        <Route path="/app/dataroom/company/list" component={CompanyDataRoomList} />
+        <Route path="/app/dataroom/detail" component={DataRoom} />
+        <Route path="/app/dataroom/company/detail" component={CompanyDataRoomDetail} />
+        <Route path="/app/timeline/list" component={OrgBdTableList} />
+        <Route path="/app/email/list" component={EmailList} />
+        <Route path="/app/email/detail/:id" component={EmailDetail} />
+        <Route path="/app/user/edit/:id" component={EditUser} />
+        <Route path="/app/user/:id" component={UserDetail} />
+        <Route path="/app/timeline/add" component={AddTimeline} />
+        <Route path="/app/timeline/edit/:id" component={EditTimeline} />
+        <Route path="/app/timeline/:id" component={TimelineDetail} />
+        <Route path={URI_8} component={InboxList} />
+        <Route path="/app/dataroom/add" component={AddDataRoom} />
+        <Route path="/app/marketplace/add" component={AddMarketPlace} />
+        <Route path="/app/marketplace/edit/:id" component={EditMarketPlace} />
+        <Route path="/app/marketplace/:id" component={MarketPlaceDetail} />
+        <Route path={URI_12} component={MyInvestor} />
+        <Route path={URI_13} component={MyTrader} />
+        <Route path="/app/trader/add" component={SelectTraderToRelation} />
+        <Route path="/app/agreement" component={Agreement} />
+        <Route path="/app/wxmsg" component={WxMessage} />
+        <Route path="/app/schedule" component={Schedule} />
+        <Route path="/app/schedule/list" component={ScheduleList} />
+        <Route path="/password" component={ResetPassword} />
+        <Route path="/app/org/bd" component={OrgBDList} />
+        <Route path="/app/org/newbd" component={NewOrgBDNext} />
+        <Route path="/app/orgbd/add" component={NewOrgBD} />
+        <Route path="/app/trader/:id" component={TraderDetail} />
+        <Route path="/app/meeting/bd" component={MeetingBDList} />
+        <Route path="/app/meetingbd/add" component={AddMeetingBD} />
+        <Route path="/app/org/export" component={OrgExportList} />
+        <Route path="/app/orgbd/project/list" component={OrgBDProjList} />
+        <Route path="/app/report/add" component={AddReport} />
+        <Route path="/app/report/list" component={ReportList} />
+        <Route path="/app/report/:id" component={ReportDetail} />
+        <Route path="/app/report/edit/:id" component={EditReport} />
+        <Route path="/app/okr/add" component={AddOKR} />
+        <Route path="/app/okr/list" component={OKRList} />
+        <Route path="/app/okr/edit/:id" component={EditOKR} />
+        <Route path="/app/online-test" component={InternOnlineTest} />
+        <Route path="/app/project-report" component={ProjectReport} /> */}
+      </Switch>
     </Router>
   )
 }
