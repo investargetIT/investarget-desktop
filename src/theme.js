cf7645fc071bd1a43f6a95f5efca70de24a26777
InvesTarget <system@investarget.com>
2021-03-12 11:13:10 +0800

    Remove theme styles

--- src/theme.js
+++ src/theme.js
@@ -3,34 +3,34 @@ var theme = {
 
 
   // Colors
-  "primary-color": "#428BCA",
-  "success-color": "#1CAF9A",
-  "warning-color": "#F0AD4E",
-  "error-color": "#D9534F",
+  // "primary-color": "#428BCA",
+  // "success-color": "#1CAF9A",
+  // "warning-color": "#F0AD4E",
+  // "error-color": "#D9534F",
 
-  "body-background": "#f2f2f2",
-  "text-color": "#636e7b",
-  "font-size-base": "14px",
-  "line-height-base": 1.5,
+  // "body-background": "#f2f2f2",
+  // "text-color": "#636e7b",
+  // "font-size-base": "14px",
+  // "line-height-base": 1.5,
 
-  "border-color-base": "#ccc",
+  // "border-color-base": "#ccc",
 
   // Layout
-  "layout-body-background": "#f2f2f2",
+  // "layout-body-background": "#f2f2f2",
 
   // Buttons
-  "btn-font-weight": "400",
-  "btn-border-radius-base": "3px",
+  // "btn-font-weight": "400",
+  // "btn-border-radius-base": "3px",
 
   // Input
-  "input-color": "#555",
-  "input-hover-border-color": "#999",
+  // "input-color": "#555",
+  // "input-hover-border-color": "#339bd2",
 
   // Outline
-  "outline-color": "#999",
+  // "outline-color": "#999",
 
   // Checkbox
-  "checkbox-size": "18px",
+  // "checkbox-size": "18px",
 }
 
 module.exports = theme
