a4906c3e3b6a8fd3b5c0e715876a3a0b30034094
InvesTarget <system@investarget.com>
2021-05-16 13:30:22 +0800

    Keep work on the previous task

--- public/pdf_viewer/statistic.js
+++ public/pdf_viewer/statistic.js
@@ -1,1 +1,66 @@
-console.log('hello, statistics');
+console.log('hello statistic');
+
+const statisticQueryString = window.location.search;
+const statisticUrlParams = new URLSearchParams(statisticQueryString);
+// const dataroomId = urlParams.get('dataroomId');
+const statisticFileId = statisticUrlParams.get('fileId');
+console.log('file id', statisticFileId);
+// const documentId = fileId;
+
+const statisticBaseUrl = 'http://apitest.investarget.com';
+
+function statisticGetUserInfo() {
+  const userInfoStr = localStorage.getItem('user_info');
+  try {
+    return JSON.parse(userInfoStr);
+  } catch(e) {
+    return null;
+  }
+}
+
+const readFileRequest = async (documentId) => {
+  if (!statisticFileId) return;
+
+  console.log('request begin to read file');
+  return;
+
+  const user = getUserInfo()
+  if (!user) {
+    throw new Error('user missing');
+  }
+
+  const source = parseInt(localStorage.getItem('source'), 10)
+  if (!source) {
+    throw new Error('data source missing');
+  }
+
+  const body = {
+    file: parseInt(documentId),
+    type: 0,
+  };
+
+  const reqDiscussion = await fetch(`${baseUrl}/dataroom/userRecord/`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'clienttype': '3',
+      'source': source,
+      'token': user.token,
+    },
+    body: JSON.stringify(body),
+  });
+  const response = await reqDiscussion.json();
+  console.log('req read file', response);
+  const { code } = response;
+  if (code !== 1000) {
+    if (response.errormsg) {
+      alert(response.errormsg);
+    } else {
+      alert('未知错误');
+    }
+  }
+}
+
+readFileRequest(fileId);
