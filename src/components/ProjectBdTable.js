a707b4f97f838c58625145123fba8d1e1553b8f1
InvesTarget <system@investarget.com>
2021-05-25 16:41:46 -0400

    Request data

--- src/components/ProjectBdTable.js
+++ src/components/ProjectBdTable.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function() {
+
+  const [projBdList, setProjBdList] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const params = {
+        page_index: 3,
+        page_size: 8,
+      }
+      const req = await api.getProjBDList(params);
+      setProjBdList(req.data.data);
+    }
+    fetchData();
+  }, []);
+
   return <h1>Project BD</h1>;
 }
