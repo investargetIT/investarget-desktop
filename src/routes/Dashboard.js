265be42df3e5d8eb0ff7dfd56273ffaa733f6e88
InvesTarget <system@investarget.com>
2021-05-25 18:22:25 -0400

    Market message

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Breadcrumb, Card, Row, Col } from 'antd';
 import LeftRightLayoutPure from '../components/LeftRightLayoutPure';
-import { getUserInfo } from '../utils/util';
+import { getUserInfo, i18n } from '../utils/util';
 import ProjectCard from '../components/ProjectCard';
 import * as api from '../api';
 import { connect } from 'dva';
@@ -12,6 +12,7 @@ function Dashboard(props) {
   const userInfo = getUserInfo();
 
   const [projList, setProjList] = useState([]);
+  const [news, setNews] = useState();
 
   useEffect(() => {
     props.dispatch({ type: 'app/getSource', payload: 'country' });
@@ -26,6 +27,20 @@ function Dashboard(props) {
       setProjList(projList);
     }
     fetchData();
+
+    async function fetchNewsData() {
+      const reqNews = await api.getWxMsg({ isShow: true });
+      if (reqNews.data.count > 0) {
+        setNews(reqNews.data.data[0]);
+      } else {
+        setNews({
+          createtime: '2018-06-26T17:18:24.086778',
+          content: '兴旺投资创始合伙人黎媛菲：主要看消费、教育和科技。消费主要是包装食品、宠物、互联网餐饮、内衣、直播电商等新消费新零售赛道都很积极。科技主要是5G 半导体，大数据云计算、工业互联网。',
+        });
+      }
+    }
+    fetchNewsData();
+
   }, []);
 
   return (
@@ -62,12 +77,16 @@ function Dashboard(props) {
           </Col>
 
           <Col span={8}>
-            <Card title="市场消息" style={{ marginBottom: 20 }} extra={<a href="#">查看更多</a>}>
-              <div style={{ height: 100 }}>Card content</div>
+
+            <Card title="市场消息" style={{ marginBottom: 20, fontSize: 14, lineHeight: '22px' }} extra={<a href="#">查看更多</a>}>
+              <div style={{ color: '#262626' }}>{news ? news.content : i18n('no_news')}</div>
+              {news && <div style={{ marginTop: 10, color: '#989898' }}>发布时间：{news.createtime.slice(0, 10)}</div>}
             </Card>
+
             <Card title="公司培训文件" extra={<a href="#">全部文件</a>}>
               <div style={{ height: 73 }}>Card content</div>
             </Card>
+
           </Col>
 
         </Row>
