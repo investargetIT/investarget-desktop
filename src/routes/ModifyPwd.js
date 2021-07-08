45b680eac858ffc782590714cdfa53e4b64f65fd
InvesTarget <system@investarget.com>
2021-07-08 18:03:49 -0300

    Finish modify password

--- src/routes/ModifyPwd.js
+++ src/routes/ModifyPwd.js
@@ -9,35 +9,24 @@ import { routerRedux } from 'dva/router'
 
 function ModifyPwd(props) {
 
-  function getChildContext() {
-    return {
-      form: props.form
-    }
-  }
-
   const [form] = Form.useForm();
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    props.form.validateFieldsAndScroll((err, values) => {
-      if(!err) {
-        modifyPassword(props.currentUserID, values.old_password, values.password)
-        .then(data => {
-          message.success(i18n('account.password_updated'))
-          let url = '/app';
-          if (!isLogin().is_superuser && hasPerm('usersys.as_investor')) {
-            url = '/app/dataroom/project/list';
-          }
-          props.dispatch(routerRedux.replace(url))
+  function handleSubmit(values) {
+    modifyPassword(props.currentUserID, values.old_password, values.password)
+      .then(_ => {
+        message.success(i18n('account.password_updated'))
+        let url = '/app';
+        if (!isLogin().is_superuser && hasPerm('usersys.as_investor')) {
+          url = '/app/dataroom/project/list';
+        }
+        props.dispatch(routerRedux.replace(url))
+      })
+      .catch(err => {
+        props.dispatch({
+          type: 'app/findError',
+          payload: err
         })
-        .catch(err => {
-          props.dispatch({
-            type: 'app/findError',
-            payload: err
-          })
-        })
-      }
-    })
+      })
   }
 
   return (
@@ -45,7 +34,7 @@ function ModifyPwd(props) {
       location={props.location}
       title={i18n("account.change_password")}>
 
-      <Form form={form} style={{ width: 500, margin: '0 auto' }} onSubmit={handleSubmit}>
+      <Form form={form} style={{ width: 500, margin: '0 auto' }} onFinish={handleSubmit}>
         <OldPassword />
         <Password label={i18n("account.new_password")} />
         <ConfirmPassword formRef={form} />
