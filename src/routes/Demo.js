07857110c51d9509b299987440eaec011d33500b
InvesTarget <system@investarget.com>
2021-07-20 19:49:34 -0300

    Work on demo

--- src/routes/Demo.js
+++ src/routes/Demo.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import LeftRightLayoutPure from '../components/LeftRightLayoutPure';
 import { withRouter } from 'dva/router';
-import { Table, Tag, Tree, Button } from 'antd';
+import { Table, Tag, Cascader, Button } from 'antd';
 
 function Demo(props) {
 
@@ -27,17 +27,7 @@ function Demo(props) {
       dataIndex: 'age',
       key: 'age',
       render: (text, record) => {
-        return isEdit ? (
-          <Tree
-            checkable
-            defaultExpandedKeys={['0-0-0', '0-0-1']}
-            defaultSelectedKeys={['0-0-0', '0-0-1']}
-            defaultCheckedKeys={['0-0-0', '0-0-1']}
-            onSelect={onSelect}
-            onCheck={onCheck}
-            treeData={treeData}
-          />
-        ) : <Tag>预沟通</Tag>;
+        return isEdit ? <Cascader options={options} onChange={onChange} placeholder="Please select" /> : <Tag>预沟通</Tag>;
       },
     },
     {
@@ -103,43 +93,58 @@ function Demo(props) {
     },
   ];
 
-  const treeData = [
+  const options = [
     {
-      title: 'parent 1',
-      key: '0-0',
+      value: '暂未联系',
+      label: '暂未联系',
+    },
+    {
+      value: '预沟通',
+      label: '预沟通',
       children: [
         {
-          title: 'parent 1-0',
-          key: '0-0-0',
-          disabled: true,
-          children: [
-            {
-              title: 'leaf',
-              key: '0-0-0-0',
-              disableCheckbox: true,
-            },
-            {
-              title: 'leaf',
-              key: '0-0-0-1',
-            },
-          ],
+          value: '无材料',
+          label: '无材料',
         },
         {
-          title: 'parent 1-1',
-          key: '0-0-1',
-          children: [{ title: <span style={{ color: '#1890ff' }}>sss</span>, key: '0-0-1-0' }],
+          value: 'Teaser',
+          label: 'Teaser',
+        },
+        {
+          value: 'BP',
+          label: 'BP',
+        },
+        {
+          value: 'DP',
+          label: 'DP',
+        },
+        {
+          value: '补充材料',
+          label: '补充材料',
+        },
+      ],
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu',
+      children: [
+        {
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [
+            {
+              value: 'zhonghuamen',
+              label: 'Zhong Hua Men',
+            },
+          ],
         },
       ],
     },
   ];
 
-  const onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
-  };
-
-  const onCheck = (checkedKeys, info) => {
-    console.log('onCheck', checkedKeys, info);
-  };
+  function onChange(value) {
+    console.log(value);
+  }
 
 	return (
     <LeftRightLayoutPure location={props.location}>
