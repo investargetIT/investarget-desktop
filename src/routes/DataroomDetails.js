9ce778b5b04e23ecaa10a403c2c9f5a85dd97fa0
InvesTarget <system@investarget.com>
2021-09-02 16:35:09 -0300

    Dataroom

--- src/routes/DataroomDetails.js
+++ src/routes/DataroomDetails.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
 import LeftRightLayoutPure from '../components/LeftRightLayoutPure';;
-import { Breadcrumb, Button, Card } from 'antd';
+import { Breadcrumb, Button, Card, Modal, Select, Input } from 'antd';
 import { getURLParamValue, handleError, hasPerm, isLogin, i18n } from '../utils/util';
 import { SelectExistInvestor } from '../components/ExtraInput';
 import * as api from '../api';
 import { PlusOutlined } from '@ant-design/icons';
+import _ from 'lodash';
+
+const { Option } = Select;
 
 function DataroomDetails(props) {
 
@@ -24,6 +27,144 @@ function DataroomDetails(props) {
   const [makeUserIds, setMakeUserIds] = useState([]);
   const [hasPermissionForDataroomTemp, setHasPermissionForDataroomTemp] = useState(false);
   const [newUser, setNewUser] = useState(null);
+  const [showDataRoomTempModal, setShowDataRoomTempModal] = useState(false);
+  const [dataRoomTemp, setDataRoomTemp] = useState([]);
+  const [selectedDataroomTemp, setSelectedDataroomTemp] = useState('');
+  const [list, setList] = useState([]);
+  const [dataRoomTempModalUserId, setDataRoomTempModalUserId] = useState('');
+  const [pdfPasswordForTemp, setPdfPasswordForTemp] = useState('');
+  const [pdfPassword, setPdfPassword] = useState('');
+  const [currentBD, setCurrentBD] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [dataroomUsersOrgBdByOrg, setDataroomUsersOrgBdByOrg] = useState([]);
+  const [userWithNewDataroomFile, setUserWithNewDataroomFile] = useState([]);
+  const [userOptions, setUserOptions] = useState([]);
+  const [userDataroomIds, setUserDataroomIds] = useState([]);
+  const [userDataroomMap, setUserDataroomMap] = useState({});
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [fileUserList, setFileUserList] = useState([]);
+  const [targetUserFileList, setTargetUserFileList] = useState([]);
+
+  async function getOrgBdOfUsers(users) {
+    const pageSize = 100;
+    let result = await api.getOrgBdList({
+      bduser: users.map(m => m.id),
+      proj: projectID,
+      page_size: pageSize,
+    });
+    const { count } = result.data;
+    if (count > pageSize) {
+      result = await api.getOrgBdList({
+        bduser: users.map(m => m.id),
+        proj: projectID,
+        page_size: count,
+      });
+    }
+
+    if (currentBD) {
+      const comments = result.data.data.filter(item => item.id == currentBD.id)[0].BDComments || [];
+      setComments(comments);
+    }
+
+    let dataroomUserOrgBdBeforeSort = users.map(m => {
+      const { org } = m;
+      if (org) {
+        return { id: org.id, org, orgbd: [] };
+      } else {
+        return { id: -1, org: { id: -1, orgname: '暂无机构' }, orgbd: [] };
+      }
+    });
+    dataroomUserOrgBdBeforeSort = _.uniqBy(dataroomUserOrgBdBeforeSort, 'id');
+  
+    // 暂无机构排最后
+    const result1 = [], result2 = [];
+    for (let index = 0; index < dataroomUserOrgBdBeforeSort.length; index++) {
+      const element = dataroomUserOrgBdBeforeSort[index];
+      if (element.id !== -1) {
+        result1.push(element);
+      } else {
+        result2.push(element);
+      }
+    }
+    const dataroomUserOrgBd = result1.concat(result2);
+
+    const allOrgBds = result.data.data;
+    for (let index = 0; index < allOrgBds.length; index++) {
+      const element = allOrgBds[index];
+      const { org } = element;
+      const orgID = org ? org.id : -1;
+      const orgIndex = dataroomUserOrgBd.map(m => m.org.id).indexOf(orgID);
+      if (orgIndex > -1) {
+        dataroomUserOrgBd[orgIndex].orgbd.push(element);
+      } else {
+        dataroomUserOrgBd.push({ id: org.id, org, orgbd: [element] });
+      }
+    }
+    setDataroomUsersOrgBdByOrg(dataroomUserOrgBd); 
+  }
+
+  async function checkUserNewFile(userIds) {
+    const res = await Promise.all(userIds.map(m => api.getNewDataroomFile(dataroomID, m)));
+    let result = [];
+    for (let index = 0; index < res.length; index++) {
+      const element = res[index];
+      if (element.data.length > 0) {
+        result.push(userIds[index]);
+      }
+    }
+    setUserWithNewDataroomFile(result);
+  }
+
+  function getAllUserFile() {
+    api.queryUserDataRoom({ dataroom: dataroomID }).then(result => {
+      const list = result.data.data
+      
+      if (list.length > 0) {
+        getOrgBdOfUsers(list.map(m => m.user));
+      }
+
+      const users = list.map(item => item.user)
+      const userIds = users.map(item => item.id)
+      checkUserNewFile(userIds);
+      const userDataroomIds = list.map(item => item.id)
+      var userDataroomMap = {}
+      userIds.forEach((userId, index) => {
+        userDataroomMap[userId] = userDataroomIds[index]
+      })
+      const userOptions = users.map(item => ({ label: item.username, value: item.id }));
+
+      setList(list);
+      setDataRoomTempModalUserId(list.length > 0 ? '' + list[0].user.id : '');
+      setUserOptions(userOptions);
+      setUserDataroomIds(userDataroomIds);
+      setUserDataroomMap(userDataroomMap);
+
+      if (selectedUser && !userIds.includes(selectedUser)) {
+        setSelectedUser(null);
+      }
+      
+      return Promise.all(list.map((item) => {
+        return api.getUserDataroomFile(dataroomID, item.user.id).then((result1) => {
+          const { data } = result1.data;
+          return data.filter(f => f.file !== null).map((m) => {
+            return { id: m.id, dataroomUserfileId: item.id, file: m.file.id, user: item.user.id };
+          });
+        });
+      }))
+
+      .then(results => {
+        const list = results.reduce((a,b) => a.concat(b), [])
+        setFileUserList(list);
+        if (selectedUser) {
+          let _list = list.filter(item => item.user == selectedUser)
+          setTargetUserFileList(_list);
+        }
+      })
+
+    }).catch(error => {
+      handleError(error)
+    })
+  }
 
   useEffect(() => {
     props.dispatch({ type: 'app/getSource', payload: 'orgbdres' });
@@ -41,12 +182,29 @@ function DataroomDetails(props) {
         }
       }).catch(handleError);
     }
+
+    async function getDataRoomTemp() {
+      const reqDataroomTemp = await api.getDataroomTemp({ dataroom: dataroomID });
+      let allUserInfo = [];
+      const allTempUserIds = reqDataroomTemp.data.data.map(m => m.user);
+      if (allTempUserIds.length > 0) {
+        const allUsersReq = await api.batchGetUserSimpleInfo({ id: allTempUserIds, page_size: allTempUserIds.length });
+        allUserInfo = allUsersReq.data.data;
+      }
+      const dataRoomTemplate = reqDataroomTemp.data.data.map((m, i) => ({ ...m, userInfo: allUserInfo.filter(f => f.id === m.user)[0] }));
+ 
+      setDataRoomTemp(dataRoomTemplate);
+      setSelectedDataroomTemp(dataRoomTemplate.length > 0 ? '' + dataRoomTemplate[0].id : '');
+      setPdfPassword(dataRoomTemplate.length > 0 ? dataRoomTemplate[0].password : '');
+      setPdfPasswordForTemp(dataRoomTemplate.length > 0 ? dataRoomTemplate[0].password : '');
+    }
+
     getProjectDetails();
+    getDataRoomTemp();
+    getAllUserFile();
 
     // this.getDataRoomFile()
     // this.getDataRoomFileAnnotations(); 
-    // this.getAllUserFile()
-    // this.getDataRoomTemp();
     // if (!isLogin().is_superuser && hasPerm('usersys.as_investor')) {
     //   this.getNewDataRoomFile();
     // }
@@ -54,12 +212,6 @@ function DataroomDetails(props) {
     
 
   }, []);
-
-  function onApplyTemplate() {
-    // this.setState({ showDataRoomTempModal: true });
-    window.echo('handle applydd');
-    // TODO
-  }
 
   function handleAddUser() {
 
@@ -80,6 +232,10 @@ function DataroomDetails(props) {
     // })
   }
 
+  function handleConfirmSelectDataroomTemp() {
+
+  }
+
   return (
     <LeftRightLayoutPure location={props.location}>
       <Breadcrumb style={{ marginLeft: 20, marginBottom: 20 }}>
@@ -97,7 +253,7 @@ function DataroomDetails(props) {
 
       <Card>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          {hasPermissionForDataroomTemp && <Button style={{ width: 109, height: 32 }} onClick={onApplyTemplate}>应用模版</Button>}
+          {hasPermissionForDataroomTemp && <Button style={{ width: 109, height: 32 }} onClick={() => setShowDataRoomTempModal(true)}>应用模版</Button>}
           {isAbleToAddUser &&
             <div>
               <div style={{ display: 'flex' }}>
@@ -116,6 +272,53 @@ function DataroomDetails(props) {
           }
         </div>
       </Card>
+
+      {showDataRoomTempModal &&
+        <Modal
+          title="选择Dataroom模版"
+          visible={true}
+          onOk={handleConfirmSelectDataroomTemp}
+          onCancel={() => setShowDataRoomTempModal(false)}
+        >
+          <div>
+
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <div>
+                <Select
+                  defaultValue={dataRoomTemp.length > 0 ? '' + dataRoomTemp[0].id : undefined}
+                  style={{ width: 120 }}
+                  onChange={value => setSelectedDataroomTemp(value)}
+                  value={selectedDataroomTemp + ''}
+                >
+                  {dataRoomTemp.map(m => <Option key={m.id} value={m.id + ''}>{m.userInfo.username}</Option>)}
+                </Select>
+              </div>
+              <div style={{ margin: '0 10px' }}>应用到</div>
+              <div>
+                <Select
+                  defaultValue={list.length > 0 ? '' + list[0].user.id : undefined}
+                  style={{ width: 120 }}
+                  onChange={value => setDataRoomTempModalUserId(value)}
+                  value={dataRoomTempModalUserId + ''}
+                >
+                  {list.map(m => m.user).map(m => <Option key={m.id} value={m.id + ''}>{m.username}</Option>)}
+                </Select>
+              </div>
+            </div>
+
+            <div style={{ marginTop: 20, display: 'grid', width: 400, gridTemplateColumns: 'auto 1fr' }}>
+              <div style={{ alignSelf: 'center', marginRight: 10 }}>设置pdf默认编辑密码</div>
+              <Input
+                placeholder="不输入该密码，pdf文件将默认不加密"
+                value={pdfPasswordForTemp}
+                onChange={e => setPdfPasswordForTemp(e.target.value)}
+              />
+              <div style={{ gridColumn: 2, fontSize: 12, fontStyle: 'oblique' }}>该密码仅针对pdf文件有效</div>
+            </div>
+
+          </div>
+        </Modal>
+      }
 
     </LeftRightLayoutPure>
   );
