0b4f0c73e299712ac86e91cc71f607e90d5ce3b1
InvesTarget <system@investarget.com>
2021-09-30 09:56:22 -0300

    Fix an issue

--- src/routes/PersonalCenter.js
+++ src/routes/PersonalCenter.js
@@ -2,7 +2,10 @@ import React from 'react';
 import LeftRightLayoutPure from '../components/LeftRightLayoutPure';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
-import { Breadcrumb, Row, Col, Card } from 'antd';
+import { Breadcrumb, Card } from 'antd';
+import {
+  ManOutlined,
+} from '@ant-design/icons';
 
 function PersonalCenter(props) {
   return (
@@ -22,7 +25,7 @@ function PersonalCenter(props) {
             <div style={{ textAlign: 'center' }}>
               <img style={{ width: 100, height: 100, borderRadius: '50%' }} src="/images/avatar1.png" />
             </div>
-            <div style={{ marginTop: 12, fontSize: 20, textAlign: 'center', lineHeight: '28px', color: 'rgba(0, 0, 0, .85)', fontWeight: 500 }}>Vincent Chen</div>
+            <div style={{ marginTop: 12, fontSize: 20, textAlign: 'center', lineHeight: '28px', color: 'rgba(0, 0, 0, .85)', fontWeight: 500 }}>Vincent Chen <ManOutlined style={{ color: '#339bd2', marginLeft: 4, fontSize: 18 }}/></div>
             <div style={{ marginTop: 20, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>职位：</span>高级投资经理</div>
             <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>部门：</span>战略投资</div>
             <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>部门主管：</span>Eric Shen</div>
@@ -31,7 +34,7 @@ function PersonalCenter(props) {
           </Card>
         </div>
         <div style={{ flex: 1 }}>
-          <Card>cc</Card>
+          <Card></Card>
         </div>
       </div>
 
