87bc645082b1ed81a76ea6dab770a1d1ac6294cc
InvesTarget <system@investarget.com>
2021-10-01 16:51:48 -0300

    Keep working on personal center

--- src/routes/PersonalCenter.js
+++ src/routes/PersonalCenter.js
@@ -21,16 +21,69 @@ function PersonalCenter(props) {
 
       <div style={{ display: 'flex' }}>
         <div style={{ width: 280, marginRight: 20 }}>
-          <Card bodyStyle={{ padding: '40px 60px' }}>
-            <div style={{ textAlign: 'center' }}>
-              <img style={{ width: 100, height: 100, borderRadius: '50%' }} src="/images/avatar1.png" />
+          <Card bodyStyle={{ padding: 0 }}>
+            <div style={{ padding: '40px 60px' }}>
+              <div style={{ textAlign: 'center' }}>
+                <img style={{ width: 100, height: 100, borderRadius: '50%' }} src="/images/avatar1.png" />
+              </div>
+              <div style={{ marginTop: 12, fontSize: 20, textAlign: 'center', lineHeight: '28px', color: 'rgba(0, 0, 0, .85)', fontWeight: 500 }}>Vincent Chen <ManOutlined style={{ color: '#339bd2', marginLeft: 4, fontSize: 18 }} /></div>
+              <div style={{ marginTop: 20, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>职位：</span>高级投资经理</div>
+              <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>部门：</span>战略投资</div>
+              <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>部门主管：</span>Eric Shen</div>
+              <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>直属上级：</span>Amy Zhao</div>
+              <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>入职日期：</span>2020.10.01</div>
             </div>
-            <div style={{ marginTop: 12, fontSize: 20, textAlign: 'center', lineHeight: '28px', color: 'rgba(0, 0, 0, .85)', fontWeight: 500 }}>Vincent Chen <ManOutlined style={{ color: '#339bd2', marginLeft: 4, fontSize: 18 }}/></div>
-            <div style={{ marginTop: 20, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>职位：</span>高级投资经理</div>
-            <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>部门：</span>战略投资</div>
-            <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>部门主管：</span>Eric Shen</div>
-            <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>直属上级：</span>Amy Zhao</div>
-            <div style={{ marginTop: 8, fontSize: 14, lineHeight: '22px', color: '#595959' }}><span style={{ color: '#262626' }}>入职日期：</span>2020.10.01</div>
+            <div style={{ width: 240, margin: '0 auto', marginBottom: 100, padding: '20px 0', borderTop: '1px solid #E6E6E6' }}>
+              <div style={{ marginBottom: 20, fontSize: 14, lineHeight: '20px', fontWeight: 'bold', color: 'rgba(0, 0, 0, .85)' }}>基本信息</div>
+              <div style={{ marginBottom: 12, fontSize: 14, lineHeight: '22px', display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ color: '#262626', display: 'flex', alignItems: 'center' }}>
+                  <img style={{ marginRight: 8 }}  src="/images/birthday.svg" />
+                  <div>出生日期</div>
+                </div>
+                <div style={{ color: '#595959' }}>1990.10.01</div>
+              </div>
+
+              <div style={{ marginBottom: 12, fontSize: 14, lineHeight: '22px', display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ color: '#262626', display: 'flex', alignItems: 'center' }}>
+                  <img style={{ marginRight: 8 }}  src="/images/school.svg" />
+                  <div>毕业学校</div>
+                </div>
+                <div style={{ color: '#595959' }}>上海交通大学</div>
+              </div>
+
+              <div style={{ marginBottom: 12, fontSize: 14, lineHeight: '22px', display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ color: '#262626', display: 'flex', alignItems: 'center' }}>
+                  <img style={{ marginRight: 8 }}  src="/images/education.svg" />
+                  <div>学历</div>
+                </div>
+                <div style={{ color: '#595959' }}>学士</div>
+              </div>
+
+              <div style={{ marginBottom: 12, fontSize: 14, lineHeight: '22px', display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ color: '#262626', display: 'flex', alignItems: 'center' }}>
+                  <img style={{ marginRight: 8 }}  src="/images/profession.svg" />
+                  <div>专业</div>
+                </div>
+                <div style={{ color: '#595959' }}>工商管理</div>
+              </div>
+
+              <div style={{ marginBottom: 12, fontSize: 14, lineHeight: '22px', display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ color: '#262626', display: 'flex', alignItems: 'center' }}>
+                  <img style={{ marginRight: 8 }}  src="/images/specialty.svg" />
+                  <div>特长爱好</div>
+                </div>
+                <div style={{ color: '#595959' }}>打球、听音乐</div>
+              </div>
+
+              <div style={{ marginBottom: 12, fontSize: 14, lineHeight: '22px', display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ color: '#262626', display: 'flex', alignItems: 'center' }}>
+                  <img style={{ marginRight: 8 }}  src="/images/others.svg" />
+                  <div>其他</div>
+                </div>
+                <div style={{ color: '#595959' }}>暂无</div>
+              </div>
+
+            </div>
           </Card>
         </div>
         <div style={{ flex: 1 }}>
