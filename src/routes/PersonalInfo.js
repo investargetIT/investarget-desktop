d85faef2f0ee21e2a53a957d8ca5c998a506901b
InvesTarget <system@investarget.com>
2021-10-06 19:03:01 -0300

    Work on personal info

--- src/routes/PersonalInfo.js
+++ src/routes/PersonalInfo.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import LeftRightLayoutPure from '../components/LeftRightLayoutPure';
 import { connect } from 'dva';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Card, Tabs } from 'antd';
 import { Link } from 'dva/router';
 
+const { TabPane } = Tabs;
+
 function PersonalInfo(props) {
+
+  function callback(key) {
+    console.log(key);
+  }
+
 	return (
     <LeftRightLayoutPure location={props.location}>
       <Breadcrumb style={{ marginLeft: 20, marginBottom: 20 }}>
@@ -16,6 +23,17 @@ function PersonalInfo(props) {
         </Breadcrumb.Item>
         <Breadcrumb.Item>个人设置</Breadcrumb.Item>
       </Breadcrumb>
+
+      <Card bodyStyle={{ padding: '20px 0' }}>
+        <Tabs className="tabs-personal-info" defaultActiveKey="1" onChange={callback} tabPosition="left" tabBarStyle={{ width: 240 }}>
+          <TabPane tab="基本设置" key="1">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="工作经历" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+        </Tabs>
+      </Card>
     </LeftRightLayoutPure>
   )
 }
