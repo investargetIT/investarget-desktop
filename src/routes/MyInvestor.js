d4111902dbeb257992b71faeb0308325ec3e93da
InvesTarget <system@investarget.com>
2021-12-12 09:03:13 -0400

    Fix add investor

--- src/routes/MyInvestor.js
+++ src/routes/MyInvestor.js
@@ -6,13 +6,13 @@ import MyPartner from '../components/MyPartner'
 function MyInvestor(props) {
 
   const action = {
-    link: hasPerm('usersys.admin_manageuser') ? `/app/user/add?redirect=${encodeURIComponent(props.location.pathname)}` : null,
+    link: hasPerm('usersys.as_trader') ? `/app/user/add?redirect=${encodeURIComponent(props.location.pathname)}` : null,
     name: i18n('user.create_investor'),
-    style: !hasPerm('usersys.admin_manageuser') ? { color: 'gray' } : null,
+    style: !hasPerm('usersys.as_trader') ? { color: 'gray' } : null,
   }
 
   return (
-    <LeftRightLayout location={props.location} title={i18n("user.myinvestor")} action={hasPerm('usersys.admin_manageuser') ? action : undefined}>
+    <LeftRightLayout location={props.location} title={i18n("user.myinvestor")} action={hasPerm('usersys.as_trader') ? action : undefined}>
       <MyPartner type="investor" location={props.location} />
     </LeftRightLayout>
   )
