52221e808097f9f18d7c71ebe0b0f81faee7832d
InvesTarget <system@investarget.com>
2022-07-08 15:53:01 +0800

    Fix build error

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
 sed -i '' -e 's|http://apitest|https://api|' public/pdf_viewer/annotation.js
 sed -i '' -e 's|http://apitest|https://api|' public/pdf_viewer/statistic.js
+npx babel ./node_modules/docx/build/index.js --out-file ./node_modules/docx/build/index.js
 yarn build
 cp -r dist/* prod/
 cd prod
