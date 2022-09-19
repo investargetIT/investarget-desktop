5427a5c6365754c81b49ad9b42ab95c1642b2f87
InvesTarget <system@investarget.com>
2022-09-19 18:08:46 +0800

    Remove onlydataroom download restrictions

--- src/components/DataroomFileManage.js
+++ src/components/DataroomFileManage.js
@@ -799,7 +799,7 @@ function DataroomFileManage({
           <span style={{ marginRight: 4 }}>{renderFileIcon()}</span>
           <span style={{ wordBreak: 'break-all' }}>{item.title}</span>
         </div>
-        <div>{(isLogin().is_superuser || !hasPerm('usersys.as_investor')) && addOperationIcon}{(isLogin().is_superuser || !hasPerm('dataroom.onlydataroom')) && moreOperationIcon}</div>
+        <div>{(isLogin().is_superuser || !hasPerm('usersys.as_investor')) && addOperationIcon}{moreOperationIcon}</div>
       </div>
     );
   }
