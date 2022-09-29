9cd17dd7546e20eff40ccc860d4f8fc3fa99a2f1
InvesTarget <system@investarget.com>
2022-09-29 02:14:01 +0000

    Update docker

--- deploy-test-docker.sh
+++ deploy-test-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker exec -it -e NODE_ENV=development busy_chatelet yarn build
+docker exec -it -e NODE_ENV=development investarget-desktop-web yarn build
 cp -r dist/* test/
 cd test
 git add .
