5631c7b74deebcfc023624daa6c389a1738c930f
InvesTarget <system@investarget.com>
2022-12-12 16:37:36 -0800

    Calculate data max

--- src/routes/Feishu.js
+++ src/routes/Feishu.js
@@ -14,17 +14,27 @@ import {
 const data = [
   {
     label: "未覆盖",
-    value: 200,
+    value: 120,
   },
   {
     label: "有覆盖",
-    value: 398,
+    value: 180,
   },
 ];
 
 const barColors = ['rgb(34, 93,131)', 'rgb(135, 172, 193)'];
 
 function Demo(props) {
+
+  function calculateMax(dataMax) {
+    const intDigits = dataMax.toString().length;
+    let divider = 1;
+    for (let index = 0; index < intDigits - 1; index++) {
+      divider *= 10;
+    }
+    return Math.ceil(dataMax / divider) * divider;
+  }
+
   return (
     <LeftRightLayout
       location={props.location}
@@ -36,9 +46,10 @@ function Demo(props) {
         <ResponsiveContainer width="100%" height={300}>
           <BarChart
             data={data}
+            margin={{ top: 20 }}
           >
             <XAxis dataKey="label" />
-            <YAxis domain={[0, 'dataMax + 1000']} />
+            <YAxis domain={[0, calculateMax]} />
             <Bar dataKey="value" fill="#8884d8" barSize={40}>
               {
                 data.map((_, index) => (
