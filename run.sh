6b94c2287e2538c5854e555cb5c84c27e236ab1d
InvesTarget <system@investarget.com>
2024-01-29 20:52:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 docker run -it --rm \
   --name investarget-desktop-web \
   -p 8000:8000 \
-  -v .:/usr/src/app \
+  -v $(pwd):/usr/src/app \
   -v /usr/src/app/node_modules \
   investarget-desktop-frontend
   
